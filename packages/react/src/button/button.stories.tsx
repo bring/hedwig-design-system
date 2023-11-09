65b4126cfce17a936b9786389cdc8c0069f7f640
Julian Jark <julian.jark@posten.no>
2023-11-09 23:46:46 +0100

    wip

--- packages/react/src/button/button.stories.tsx
+++ packages/react/src/button/button.stories.tsx
@@ -4,36 +4,21 @@ import { Button } from "./button";
 
 const meta: Meta<typeof Button> = {
   component: Button,
-  argTypes: {
-    type: {
-      control: { type: "radio" },
-      options: ["button", "submit", "reset"],
-    },
-  },
+  // argTypes: {
+  //   type: {
+  //     control: { type: "radio" },
+  //     options: ["button", "submit", "reset"],
+  //   },
+  // },
 };
 
 export default meta;
 
 type Story = StoryObj<typeof Button>;
 
-/*
- *👇 Render functions are a framework specific feature to allow you control on how the component renders.
- * See https://storybook.js.org/docs/react/api/csf
- * to learn how to use render functions.
- */
 export const Primary: Story = {
-  render: (props) => (
-    <Button
-      {...props}
-      onClick={(): void => {
-        // eslint-disable-next-line no-alert -- alert for demo
-        alert("Hello from Turborepo!");
-      }}
-    />
-  ),
-  name: "Button",
   args: {
-    children: "Hello world",
-    type: "button",
+    children: "Primary",
+    size: "medium",
   },
 };
--- packages/react/src/button/button.tsx
+++ packages/react/src/button/button.tsx
@@ -1,21 +1,35 @@
 import * as React from "react";
+import { clsx } from "clsx";
 import { warnForStyleOverrides } from "../utils";
 import "@posten-hedwig/css/dist/button/button.css";
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  size: "small" | "medium" | "large";
 }
 
+/**
+ * Hedwig button element
+ */
 export function Button({
   children,
   style,
   className,
+  size = "medium",
   ...rest
 }: ButtonProps): JSX.Element {
   warnForStyleOverrides(style, className);
   return (
-    <button className="hwc-button hwc-button--primary" type="button" {...rest}>
+    <button
+      className={clsx(
+        "hwc-button",
+        "hwc-button--primary",
+        `hwc-button--${size}`,
+      )}
+      type="button"
+      {...rest}
+    >
       {children}
     </button>
   );
