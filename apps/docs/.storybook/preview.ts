71c577a551e4d2c56ca8cc2c74e1035101e55b10
Julian Jark <julian.jark@posten.no>
2023-11-11 00:38:30 +0100

    explicitly import tokens and css in storybook preview to make production build include them

--- apps/docs/.storybook/preview.ts
+++ apps/docs/.storybook/preview.ts
@@ -1,5 +1,10 @@
 import { withThemeByClassName } from "@storybook/addon-themes";
 
+// TODO: Storybook Production build does not pick up the css imported in the components
+// This is not a problem for the vite example app
+import "@postenbring/hedwig-tokens/dist/web/tokens.css";
+import "@postenbring/hedwig-css/dist/index.css";
+
 export default {
   decorators: [
     withThemeByClassName({
