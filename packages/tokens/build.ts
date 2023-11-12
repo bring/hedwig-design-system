19635c458eb80fed77678298c67f7d3eac1b5a04
Julian Jark <julian.jark@posten.no>
2023-11-12 17:01:59 +0100

    handle last edge cases

--- packages/tokens/build.ts
+++ packages/tokens/build.ts
@@ -1,18 +1,62 @@
 /* eslint-disable no-console -- script */
 import { readFileSync, writeFileSync } from "node:fs";
+import type { DesignTokens } from "style-dictionary-utils";
 import StyleDictionary from "style-dictionary-utils";
 import { w3cTokenJsonParser } from "style-dictionary-utils/dist/parser/w3c-token-json-parser";
 import { hedwigTypography } from "./transforms/typography";
 
-// Handle $type, $value, $description
-StyleDictionary.registerParser(w3cTokenJsonParser);
+const config = {
+  cssVariablesPrefix: "hds",
+};
+
+/**
+ * Custom tokens parser
+ * - Handle w3c token spec
+ * - Apply the type property to all tokens
+ */
+StyleDictionary.registerParser({
+  pattern: w3cTokenJsonParser.pattern,
+  parse: (options) => {
+    // Handle $type, $value, $description
+    const result = w3cTokenJsonParser.parse(options) as DesignTokens;
+
+    // Ensure type is set on all tokens,
+    // by setting the parent type on every child.
+    //
+    // This is needed because the transformer matchers don't have access to the parent types
+    // and thus will not be applied.
+    // This is a shortcoming of the current version of style-dictionary
+    function applyTypeToEachChild(obj: Partial<DesignTokens>, parentType?: string) {
+      const type =
+        (obj.type as string | undefined) ?? (obj.$type as string | undefined) ?? parentType;
+      if (obj.value !== undefined) {
+        // @ts-expect-error -- If value is set, it's safe to set type as well
+        obj.type = type;
+      }
+      for (const child of Object.values(obj)) {
+        if (typeof child === "object") {
+          applyTypeToEachChild(child, type);
+        }
+      }
+    }
+    applyTypeToEachChild(result);
+    return result;
+  },
+});
 
 // Handle typography
 StyleDictionary.registerTransform({
   name: "hedwig/typography",
   ...hedwigTypography,
 });
-const cssTransforms = [...StyleDictionary.transformGroup.css, "hedwig/typography", "shadow/css"];
+
+const cssTransforms = [
+  ...StyleDictionary.transformGroup.css,
+  "fontFamily/css",
+  "shadow/css",
+  "cubicBezier/css",
+  "hedwig/typography",
+];
 
 function buildSharedCssVariables() {
   console.log("🤖 Building shared css variables");
@@ -20,7 +64,7 @@ function buildSharedCssVariables() {
     source: ["tokens/shared.json"],
     platforms: {
       css: {
-        prefix: "hds",
+        prefix: config.cssVariablesPrefix,
         transforms: cssTransforms,
         files: [
           {
@@ -49,6 +93,9 @@ function buildBrandCssVariables() {
               filter: "isSource",
               destination: `dist/variables/${brand}.css`,
               format: "css/variables",
+              options: {
+                outputReferences: false,
+              },
             },
           ],
         },
