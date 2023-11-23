59df1473e65871cb6f366c90188f51aa4c7450e2
Vegard Aasen <vegaasen@gmail.com>
2023-11-23 11:42:29 +0100

    Remove non-required renderer-overrider

--- packages/react/src/list/link-list.stories.tsx
+++ packages/react/src/list/link-list.stories.tsx
@@ -29,7 +29,6 @@ export const LinkListStory: Story = {
     size: "medium",
     children: linkListItems(),
   },
-  render: (props) => <LinkList {...props} />,
 };
 
 const meta: Meta<typeof LinkList> = {
