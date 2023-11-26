122641e1fd065c59980add67a1cce25b85e83259
Julian Jark <julian.jark@posten.no>
2023-11-26 14:00:53 +0100

    :truck: update css references
    lightningcss outputs the compiled css in a flat directory, which is fine.

--- packages/react/src/input/index.ts
+++ packages/react/src/input/index.ts
@@ -2,6 +2,6 @@ import "@postenbring/hedwig-tokens/tokens-output/css/fonts.css";
 import "@postenbring/hedwig-tokens/tokens-output/css/tokens.css";
 import "@postenbring/hedwig-css/dist/base.css";
 
-import "@postenbring/hedwig-css/dist/input/input.css";
+import "@postenbring/hedwig-css/dist/input.css";
 
 export * from "./input";
--- packages/react/src/message/index.tsx
+++ packages/react/src/message/index.tsx
@@ -2,7 +2,7 @@ import "@postenbring/hedwig-tokens/tokens-output/css/fonts.css";
 import "@postenbring/hedwig-tokens/tokens-output/css/tokens.css";
 import "@postenbring/hedwig-css/dist/base.css";
 
-import "@postenbring/hedwig-css/dist/box/box.css";
-import "@postenbring/hedwig-css/dist/message/message.css";
+import "@postenbring/hedwig-css/dist/box.css";
+import "@postenbring/hedwig-css/dist/message.css";
 
 export * from "./message";
