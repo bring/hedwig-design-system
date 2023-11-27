ce5ca2f493c8b070aa5982c3c58f2ad941a00367
Julian Jark <julian.jark@posten.no>
2023-11-27 10:39:09 +0100

    :adhesive_bandage: fix css import path after changes to the css package

--- packages/react/src/accordion/index.tsx
+++ packages/react/src/accordion/index.tsx
@@ -2,7 +2,7 @@ import "@postenbring/hedwig-tokens/tokens-output/css/fonts.css";
 import "@postenbring/hedwig-tokens/tokens-output/css/tokens.css";
 import "@postenbring/hedwig-css/dist/base.css";
 
-import "@postenbring/hedwig-css/dist/accordion/accordion.css";
+import "@postenbring/hedwig-css/dist/accordion.css";
 
 export * from "./accordion";
 export * from "./accordion-item";
