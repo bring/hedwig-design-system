3024d5bb52b7551ff4a4c99ac5d40f537afd320d
Julian Jark <julian.jark@posten.no>
2023-11-27 17:26:41 +0100

    :recycle: update clsx usage after merge

--- packages/react/src/button/icon-button.tsx
+++ packages/react/src/button/icon-button.tsx
@@ -1,11 +1,10 @@
-import { clsx } from "clsx";
-import { t } from "@postenbring/hedwig-css/typed-classname/index.mjs";
+import { clsx } from "@postenbring/hedwig-css/typed-classname/index.mjs";
 import { BaseButton, type ButtonProps } from "./button";
 
 export type IconButtonProps = ButtonProps & { variant: "primary" | "secondary" };
 
 export function IconButton({ className, ...rest }: IconButtonProps) {
-  return <BaseButton className={clsx(t("hds-icon-button"), className)} {...rest} />;
+  return <BaseButton className={clsx("hds-icon-button", className as undefined)} {...rest} />;
 }
 
 IconButton.displayName = "IconButton";
