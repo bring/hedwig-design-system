20799dd2ff4018a88f566443e1f0bd9b1ea2f5a2
Harald Vinje <haraldvinje@gmail.com>
2023-11-27 16:04:52 +0100

    :heavy_minus_sign: Remove import of token css as it is included in css package

--- packages/react/src/message/index.tsx
+++ packages/react/src/message/index.tsx
@@ -1,5 +1,3 @@
-import "@postenbring/hedwig-tokens/tokens-output/css/fonts.css";
-import "@postenbring/hedwig-tokens/tokens-output/css/tokens.css";
 import "@postenbring/hedwig-css/dist/base.css";
 
 import "@postenbring/hedwig-css/dist/box.css";
