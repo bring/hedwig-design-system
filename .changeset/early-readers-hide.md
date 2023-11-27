811c3b0848e638f3d7387da00e720e44f43967ed
Julian Jark <julian.jark@posten.no>
2023-11-27 23:23:07 +0100

    :lipstick: style improvements to `Message`

---
+++ .changeset/early-readers-hide.md
@@ -0,0 +1,6 @@
+---
+"@postenbring/hedwig-react": patch
+"@postenbring/hedwig-css": patch
+---
+
+:lipstick: add `Message.Title` and `Message.Description` helper components to `Message` & Fix 2px missing margin for the first text so it matches the icon
--- packages/react/src/message/index.tsx
+++ packages/react/src/message/index.tsx
@@ -5,4 +5,17 @@ import "@postenbring/hedwig-css/dist/base.css";
 import "@postenbring/hedwig-css/dist/box.css";
 import "@postenbring/hedwig-css/dist/message.css";
 
-export * from "./message";
+import { Message, MessageTitle, MessageDescription } from "./message";
+
+const MessageComponent = Message as typeof Message & {
+  Title: typeof MessageTitle;
+  Description: typeof MessageDescription;
+};
+
+MessageComponent.Title = MessageTitle;
+MessageComponent.Description = MessageDescription;
+
+MessageComponent.Title.displayName = "Message";
+MessageComponent.Description.displayName = "Message";
+
+export { MessageComponent as Message, MessageTitle, MessageDescription };
