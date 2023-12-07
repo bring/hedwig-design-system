760f9d39c84f29a75943ca006c70b6c84c2cbda6
Julian Jark <julian.jark@posten.no>
2023-12-07 19:03:11 +0100

    :memo: update example

--- apps/example/src/tailwind/app.tsx
+++ apps/example/src/tailwind/app.tsx
@@ -4,7 +4,7 @@ import { Link } from "@postenbring/hedwig-react";
 export default function TailwindExample() {
   return (
     <div>
-      <div className="p-[64px]">
+      <div className="p-large-2">
         <div className="bg-lighter rounded p-medium-3">
           <h2 className="text-h1 text-signature">Tittle</h2>
           <p className="text-body mb-medium-2">Inside some kinda card</p>
@@ -14,22 +14,24 @@ export default function TailwindExample() {
         </div>
       </div>
 
-      <div>
-        <p className="text-h1-display">Display</p>
-        <p className="text-h1">Heading 1</p>
-        <p className="text-h2">Heading 2</p>
-        <p className="text-h3">Heading 3</p>
-        <p className="text-body">Body</p>
-        <p className="text-body-small">Body small</p>
-        <p className="text-technical">Technical</p>
-        <p className="text-caption">Caption</p>
-      </div>
-      <div>
-        <p className="text-h3-title">Heading 3 Title</p>
-        <p className="text-body-title">Body Title</p>
-        <p className="text-body-small-title">Body small Title</p>
-        <p className="text-technical-title">Technical Title</p>
-        <p className="text-caption-title">Caption Title</p>
+      <div className="p-large-1 flex flex-col gap-small-4">
+        <div>
+          <p className="text-h1-display">Display</p>
+          <p className="text-h1">Heading 1</p>
+          <p className="text-h2">Heading 2</p>
+          <p className="text-h3">Heading 3</p>
+          <p className="text-body">Body</p>
+          <p className="text-body-small">Body small</p>
+          <p className="text-technical">Technical</p>
+          <p className="text-caption">Caption</p>
+        </div>
+        <div>
+          <p className="text-h3-title">Heading 3 Title</p>
+          <p className="text-body-title">Body Title</p>
+          <p className="text-body-small-title">Body small Title</p>
+          <p className="text-technical-title">Technical Title</p>
+          <p className="text-caption-title">Caption Title</p>
+        </div>
       </div>
     </div>
   );
