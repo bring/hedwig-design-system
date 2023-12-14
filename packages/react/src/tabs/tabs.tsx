5338bb1da703cff400fb13f8eee57899dad3e221
Vegard Aasen <vegaasen@gmail.com>
2023-12-14 22:34:27 +0100

    🐛Fix minor linting issues

--- packages/react/src/tabs/tabs.tsx
+++ packages/react/src/tabs/tabs.tsx
@@ -7,7 +7,7 @@ import type { TabContentsProps } from "./tabs-content";
 import type { TabListProps } from "./tabs-list";
 
 export interface TabsProps extends HTMLAttributes<HTMLElement> {
-  children: ReactElement<TabListProps | TabContentsProps>[];
+  children: ReactElement<TabListProps | TabContentsProps>[] | ReactElement;
 
   /**
    * Define which tab to use as default. Must be one of the <Tab/>s identifier.
@@ -17,10 +17,12 @@ export interface TabsProps extends HTMLAttributes<HTMLElement> {
 
 export const Tabs: OverridableComponent<TabsProps, HTMLDivElement> = forwardRef(
   ({ as: Component = "div", defaultTab, children, ...rest }, ref) => {
-    const [activeTabId, toggleActiveTabId] = useState<string>(defaultTab);
+    const [activeTabId, setActiveTabId] = useState<string>(defaultTab);
     return (
       <Component className={clsx("hds-tabs")} ref={ref} {...rest}>
-        <TabsContext.Provider value={{ activeTabId, toggleActiveTabId, mounted: true }}>
+        <TabsContext.Provider
+          value={{ activeTabId, toggleActiveTabId: setActiveTabId, mounted: true }}
+        >
           {children}
         </TabsContext.Provider>
       </Component>
