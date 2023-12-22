87106478f54ef578a5c5756cc3509449fda6230e
Julian Jark <julian.jark@posten.no>
2023-12-22 15:06:59 +0100

    :wrench: cleanup styelint config a little

--- packages/css/stylelint.config.cjs
+++ packages/css/stylelint.config.cjs
@@ -3,11 +3,7 @@
  * @type {Config}
  */
 module.exports = {
-  extends: [
-    "stylelint-config-standard",
-    "stylelint-config-standard-scss",
-    "stylelint-config-prettier-scss",
-  ],
+  extends: ["stylelint-config-standard", "stylelint-config-prettier"],
   plugins: ["stylelint-value-no-unknown-custom-properties"],
   rules: {
     // Make sure we don't misspell the tokens
@@ -20,9 +16,6 @@ module.exports = {
 
     // Recommended to be disabled when using a lot of nesting
     "no-descending-specificity": [null],
-
-    // Disabled for now, since we use both css @import url() and scss @import "" at the same time
-    "import-notation": [null],
 
     // Copied from: https://github.com/humanmade/coding-standards/issues/193#issuecomment-1405099508
     /** selector class pattern must match [BEM CSS](https://en.bem.info/methodology/css) - [Regex](https://regexr.com/3apms) */
