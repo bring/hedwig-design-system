17c03271c3032eaae9f459be43d89538cbf7af38
Harald Vinje <haraldvinje@gmail.com>
2024-01-31 15:30:16 +0100

    :bug: Try to fix brand image logo on storybook

--- apps/docs/.storybook/manager.ts
+++ apps/docs/.storybook/manager.ts
@@ -1,11 +1,12 @@
 import { addons } from "@storybook/manager-api";
 import { create } from "@storybook/theming/create";
+import postenbringLogo from "../public/postenbring-logo.png";
 
 const postenbringTheme = create({
   base: "light",
   brandTitle: "Postenbring",
   brandUrl: "https://www.postenbring.no",
-  brandImage: "../public/postenbring-logo.png",
+  brandImage: postenbringLogo,
 });
 
 addons.setConfig({
