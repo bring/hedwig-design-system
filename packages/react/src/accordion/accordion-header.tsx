f09616e55723a7fc7fd50e0a3de5a59b0569b232
Julian Jark <julian.jark@posten.no>
2024-01-31 14:26:16 +0100

    :recycle: update svg

--- packages/react/src/accordion/accordion-header.tsx
+++ packages/react/src/accordion/accordion-header.tsx
@@ -32,18 +32,9 @@ export const AccordionHeader: OverridableComponent<AccordionHeaderProps, HTMLBut
         type="button"
       >
         <span className={clsx("hds-accordion-item-header--icon")}>
-          <svg
-            fill="none"
-            height="24"
-            viewBox="0 0 24 24"
-            width="24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g className="hds-accordion-item-header--icon-chevron">
-              <path
-                className="hds-accordion-item-header--icon-chevron_down"
-                d="M19.6289 9.15625L18.9609 8.45312C18.7852 8.27734 18.5039 8.27734 18.3633 8.45312L12 14.8164L5.60156 8.45312C5.46094 8.27734 5.17969 8.27734 5.00391 8.45312L4.33594 9.15625C4.16016 9.29688 4.16016 9.57812 4.33594 9.75391L11.6836 17.1016C11.8594 17.2773 12.1055 17.2773 12.2812 17.1016L19.6289 9.75391C19.8047 9.57812 19.8047 9.29688 19.6289 9.15625Z"
-              />
+          <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+            <g>
+              <path d="M19.6289 9.15625L18.9609 8.45312C18.7852 8.27734 18.5039 8.27734 18.3633 8.45312L12 14.8164L5.60156 8.45312C5.46094 8.27734 5.17969 8.27734 5.00391 8.45312L4.33594 9.15625C4.16016 9.29688 4.16016 9.57812 4.33594 9.75391L11.6836 17.1016C11.8594 17.2773 12.1055 17.2773 12.2812 17.1016L19.6289 9.75391C19.8047 9.57812 19.8047 9.29688 19.6289 9.15625Z" />
             </g>
           </svg>
         </span>
