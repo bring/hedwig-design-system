518ce30891425e02dcacebbf7dd1bee0699e08e9
Thale Sommer <thale.sommer@kantega.no>
2024-02-29 15:14:44 +0100

    New radio button component

---
+++ packages/react/src/form/radiobutton/radiobutton.tsx
@@ -0,0 +1,33 @@
+import type { InputHTMLAttributes } from "react";
+import React, { forwardRef } from "react";
+import { clsx } from "@postenbring/hedwig-css/typed-classname/index.mjs";
+
+export interface RadiobuttonProps
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, "defaultValue"> {
+  variant?: "plain" | "bounding-box";
+  hasError?: boolean;
+  title?: string;
+}
+
+export const Radiobutton = forwardRef<HTMLInputElement, RadiobuttonProps>(
+  ({ variant = "plain", hasError, title, children, className, ...rest }, ref) => {
+    return (
+      <label
+        className={clsx(
+          "hds-radiobutton",
+          {
+            [`hds-radiobutton--${variant}`]: variant === "bounding-box",
+            "hds-radiobutton--error": hasError,
+          },
+          className as undefined,
+        )}
+      >
+        <input {...rest} ref={ref} type="radio" />
+        <span className="hds-radiobutton--checkmark" />
+        {title ? <p className="hds-radiobutton--title">{title}</p> : null}
+        {children}
+      </label>
+    );
+  },
+);
+Radiobutton.displayName = "Radiobutton";
