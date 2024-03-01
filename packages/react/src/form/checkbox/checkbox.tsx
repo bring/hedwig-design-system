1c5bc49977349a1432746ec268b7d4b785ce9e62
Thale Sommer <thale.sommer@kantega.no>
2024-03-01 14:06:36 +0100

    Set aria-invalid for checkbox when it has error.

--- packages/react/src/form/checkbox/checkbox.tsx
+++ packages/react/src/form/checkbox/checkbox.tsx
@@ -21,7 +21,7 @@ export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
           className as undefined,
         )}
       >
-        <input {...rest} ref={ref} type="checkbox" />
+        <input {...rest} aria-invalid={hasError || undefined} ref={ref} type="checkbox" />
         <span className="hds-checkbox__checkmark" />
         {title ? <p className="hds-checkbox__title">{title}</p> : null}
         {children}
