3dc1334c57f8ab49ee7c3eab6e17d07318f3c862
Henrik Hermansen <henrik.hermansen@posten.no>
2024-03-07 15:43:12 +0100

    Clarify usage of Card components
    The Card component should always be a clickable link, but the entire card should not be the actual link.
    Confused? Okay. So, to make life better for users with a screen reader, we should only make the heading
    of the card an actual link. However, we still want the entire card to be clickable for other users. We
    achieve this with some clever CSS.
    
    This also means the Card component can no longer have `as="a"`, and this is now prohibited through types.
    Futhermore the Card.Body.Header.Title component now requires an `as` prop to help HDS consumers understand
    they need to make this component a link of some sort. We also made the `as` prop required on Card.Body.Header
    to force consumers to set an appropriate heading level.
    
    We also made the little card arrow move on hover now :-)

--- packages/react/src/card/card.stories.tsx
+++ packages/react/src/card/card.stories.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies -- storybook story */
 import type { Meta, StoryObj } from "@storybook/react";
-import { Link } from "../link";
+import type { ReactNode } from "react";
 import { Card as CardComponent } from ".";
 
 const meta: Meta<typeof CardComponent> = {
@@ -14,18 +14,22 @@ type Story = StoryObj<typeof CardComponent>;
 
 export const Card: Story = {
   render: (props) => (
-    <CardComponent {...props} as="a" href="#article" style={{ maxWidth: "500px" }}>
+    <CardComponent {...props} as="div" style={{ maxWidth: "500px" }}>
       <CardComponent.Media>
         <CardComponent.Media.Img alt="posten-bring" src="./posten-bring.jpg" />
       </CardComponent.Media>
       <CardComponent.Body>
-        <CardComponent.Body.Header>
+        <CardComponent.Body.Header as="h2">
           <CardComponent.Body.Header.Overline>Theme</CardComponent.Body.Header.Overline>
-          <CardComponent.Body.Header.Title>Cool article</CardComponent.Body.Header.Title>
+          <CardComponent.Body.Header.Title as="a" href="#article">
+            Cool article
+          </CardComponent.Body.Header.Title>
         </CardComponent.Body.Header>
         <CardComponent.Body.Description>
-          This is a card as an anchor tag. You can also use the hedwig provided <code>Link</code>{" "}
-          component. The card component provides a lot of flexibility.
+          This is a card which acts as a giant link. You can also use the hedwig provided{" "}
+          <code>Link</code> component, or a link component from your routing library. The link
+          component of your choice should be set with the <code>as</code> property on the body
+          header title. The card component provides a lot of flexibility.
         </CardComponent.Body.Description>
         <CardComponent.Body.Action.Arrow />
       </CardComponent.Body>
@@ -33,44 +37,40 @@ export const Card: Story = {
   ),
 };
 
+function Link({
+  to,
+  className,
+  children,
+}: {
+  to: string;
+  className?: string;
+  children: ReactNode;
+}) {
+  return (
+    <a className={className} href={to}>
+      {children}
+    </a>
+  );
+}
+
 export const CardFullWidth: Story = {
   render: (props) => (
-    <CardComponent {...props} as="a" href="#article">
+    <CardComponent {...props} as="div">
       <CardComponent.Media>
         <CardComponent.Media.Img alt="posten-bring" src="./posten-bring.jpg" />
       </CardComponent.Media>
       <CardComponent.Body>
-        <CardComponent.Body.Header>
+        <CardComponent.Body.Header as="h2">
           <CardComponent.Body.Header.Overline>Theme</CardComponent.Body.Header.Overline>
-          <CardComponent.Body.Header.Title>Cool article</CardComponent.Body.Header.Title>
+          <CardComponent.Body.Header.Title as={Link} to="#article">
+            Cool article
+          </CardComponent.Body.Header.Title>
         </CardComponent.Body.Header>
         <CardComponent.Body.Description>
           The card will take up all available space by default. It is the consumer&apos;s choice and
           responsibility to limit the width if wanted.
         </CardComponent.Body.Description>
         <CardComponent.Body.Action.Arrow />
-      </CardComponent.Body>
-    </CardComponent>
-  ),
-};
-
-export const CardWithLink: Story = {
-  render: (props) => (
-    <CardComponent {...props} style={{ maxWidth: "500px" }}>
-      <CardComponent.Media>
-        <CardComponent.Media.Img alt="posten-bring" src="./posten-bring.jpg" />
-      </CardComponent.Media>
-      <CardComponent.Body>
-        <CardComponent.Body.Header>
-          <CardComponent.Body.Header.Overline>Theme</CardComponent.Body.Header.Overline>
-          <CardComponent.Body.Header.Title>Card with Hedwig Link</CardComponent.Body.Header.Title>
-        </CardComponent.Body.Header>
-        <CardComponent.Body.Description>
-          This card is contains a link but is not a link itself.
-        </CardComponent.Body.Description>
-        <CardComponent.Body.Action as={Link} href="#article">
-          Go to article
-        </CardComponent.Body.Action>
       </CardComponent.Body>
     </CardComponent>
   ),
