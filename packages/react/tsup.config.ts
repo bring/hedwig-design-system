1c8b8c1e27ca3e8dde2cfbba8ca7d4f1231ce518
Julian Jark <julian.jark@posten.no>
2024-03-13 09:08:21 +0100

    :hammer: move tsup config into `tsup.config.ts`
    and add react as peerDependencies

--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -2,4 +2,8 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.tsx", "src/**/*.tsx", "!src/**/*.stories.tsx"],
+  sourcemap: true,
+  format: ["cjs", "esm"],
+  dts: true,
+  external: ["react", "react-dom"],
 });
