16338bda78af50313734a077d894c237be5ae82b
Thale Sommer <thale.sommer@kantega.no>
2024-03-14 13:12:08 +0100

    Export ErrorMessage component and add stories for storybook

---
+++ packages/react/src/form/error-message/error-message.stories.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable import/no-extraneous-dependencies -- storybook story */
+import type { Meta, StoryObj } from "@storybook/react";
+import { Checkbox } from "../checkbox";
+import { ErrorMessage } from ".";
+
+const meta: Meta<typeof ErrorMessage> = {
+  title: "ErrorMessage",
+  component: ErrorMessage,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ErrorMessage>;
+
+export const Default: Story = {
+  args: {
+    id: "id",
+    children: "This is an error message for use with form input components",
+  },
+};
+
+export const CheckboxWithErrorMessage: Story = {
+  args: {
+    id: "errorMessageId",
+    children: "This checkbox has an error",
+  },
+  render: (props) => (
+    <>
+      <Checkbox hasError value="Hello" aria-describedby="errorMessageId">
+        Hello
+      </Checkbox>
+      <ErrorMessage id={props.id}>{props.children}</ErrorMessage>
+    </>
+  ),
+};
