f47d3d94a411e8d5fcd3c0cfec3c857658957e6f
Julian Jark <julian.jark@posten.no>
2024-03-15 18:24:57 +0100

    :recycle: use css inlined cheveron icon for accordion

---
+++ .changeset/tiny-maps-agree.md
@@ -0,0 +1,6 @@
+---
+"@postenbring/hedwig-react": patch
+"@postenbring/hedwig-css": patch
+---
+
+:recycle: use css inlined cheveron icon for accordion
--- packages/react/src/accordion/accordion-header.tsx
+++ packages/react/src/accordion/accordion-header.tsx
@@ -31,13 +31,6 @@ export const AccordionHeader: OverridableComponent<AccordionHeaderProps, HTMLBut
         ref={ref}
         type="button"
       >
-        <span className={clsx("hds-accordion-item-header--icon")}>
-          <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-            <g>
-              <path d="M19.6289 9.15625L18.9609 8.45312C18.7852 8.27734 18.5039 8.27734 18.3633 8.45312L12 14.8164L5.60156 8.45312C5.46094 8.27734 5.17969 8.27734 5.00391 8.45312L4.33594 9.15625C4.16016 9.29688 4.16016 9.57812 4.33594 9.75391L11.6836 17.1016C11.8594 17.2773 12.1055 17.2773 12.2812 17.1016L19.6289 9.75391C19.8047 9.57812 19.8047 9.29688 19.6289 9.15625Z" />
-            </g>
-          </svg>
-        </span>
         <span>{children}</span>
       </Component>
     );
