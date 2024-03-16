3766818bef14661b4ea0efec9bb88b19212e6eac
Julian Jark <julian.jark@posten.no>
2024-03-16 15:55:32 +0100

    :adhesive_bandage: fix css not loading when using `@postenbring/hedwig-react` during development in a fresh vite project

---
+++ .changeset/heavy-stingrays-bow.md
@@ -0,0 +1,5 @@
+---
+"@postenbring/hedwig-react": patch
+---
+
+:adhesive_bandage: fix css not loading when using `@postenbring/hedwig-react` during development in a fresh vite project
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,3 +1,11 @@
-import "@postenbring/hedwig-css";
+/**
+ * Import the hedwig css file, so the consumers don't have to. Plug and play.
+ *
+ * NOTE: Importing the full path, not just the package name.
+ * For some reason vite did not include the css during development when this file only imported the package name.
+ *
+ * Consumers using the `@postenbring/hedwig-css` package directly should only need to import the package without a path.
+ */
+import "@postenbring/hedwig-css/dist/index.css";
 
 export * from "./index-no-css";
