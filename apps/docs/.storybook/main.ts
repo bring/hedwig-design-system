eb88d74a473fa35a40240259a10ede717dbd8078
Julian Jark <julian.jark@posten.no>
2024-03-27 14:41:04 +0100

    :memo::sparkles: show examples in storybook

--- apps/docs/.storybook/main.ts
+++ apps/docs/.storybook/main.ts
@@ -1,12 +1,24 @@
 import { dirname, join } from "path";
 import type { StorybookConfig } from "@storybook/react-vite";
+import type { Indexer } from "@storybook/types";
+import type { Plugin } from "vite";
+import fs from "node:fs/promises";
 
 function getAbsolutePath(value: string) {
   return dirname(require.resolve(join(value, "package.json")));
 }
 
 const config: StorybookConfig = {
-  stories: ["../../../packages/react/src/**/*.stories.tsx"],
+  stories: ["../../../packages/react/src/**/*.stories.tsx", "../../examples/app/examples/*/*.tsx"],
+
+  // Include the examples in storybook
+  // They need a little transformation to work
+  experimental_indexers: async (existingIndexers) => [...existingIndexers!, exampleIndexer],
+  viteFinal: async (config) => {
+    config.plugins?.unshift(exampleLoader());
+    return config;
+  },
+
   staticDirs: ["../public"],
   addons: [
     getAbsolutePath("@storybook/addon-links"),
@@ -29,3 +41,59 @@ const config: StorybookConfig = {
 };
 
 export default config;
+
+const exampleFilenameRegex = /examples\/app\/examples\/.+\.tsx$/;
+const exampleIndexer: Indexer = {
+  test: exampleFilenameRegex,
+  createIndex: async (fileName) => {
+    const { componentName, exampleName } = parseExampleFilename(fileName);
+    return [
+      {
+        type: "story",
+        exportName: "default",
+        name: `Example: ${kebabCaseToFirstLetterUpperCase(exampleName)}`,
+        __id: `${componentName}--example-${exampleName}`,
+        title: kebabCaseToFirstLetterUpperCase(componentName),
+        importPath: fileName,
+      },
+    ];
+  },
+};
+
+function exampleLoader() {
+  const plugin: Plugin = {
+    name: "load-example-story",
+    async load(id) {
+      if (id.match(exampleFilenameRegex)) {
+        const { exampleName } = parseExampleFilename(id);
+        const file = await fs.readFile(id, "utf-8");
+
+        const exampleSourceNeat = file.replace(/export default.+/g, "");
+        const storyCode = `
+        ${exampleSourceNeat}
+        export const Example${kebabCaseToPascalCase(exampleName)} = {
+          render: Example
+        }
+        export default {}
+        `;
+        return storyCode;
+      }
+    },
+  };
+  return plugin;
+}
+function kebabCaseToFirstLetterUpperCase(kebabCase: string) {
+  const s = kebabCase.replace(/-/g, " ");
+  return s[0].toUpperCase() + s.slice(1);
+}
+
+function kebabCaseToPascalCase(kebabCase: string) {
+  return kebabCase.replace(/(^\w|-\w)/g, (g) => g.replace("-", "").toUpperCase());
+}
+
+function parseExampleFilename(fileName: string) {
+  const titleMatch = fileName.match(/([\w-]+)\/([\w-]+)\.tsx$/);
+  if (!titleMatch) throw new Error(`invalid file name ${fileName}`);
+  const [, componentName, exampleName] = titleMatch;
+  return { componentName, exampleName };
+}
--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -3,4 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: "@postenbring/hedwig-react",
+        replacement: new URL("../../packages/react/src", import.meta.url).pathname,
+      },
+    ],
+  },
 });
