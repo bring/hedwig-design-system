bc01ae3f18937e0b242d02c9411a47814537d97c
Julian Jark <julian.jark@posten.no>
2024-03-29 14:03:02 +0100

    :memo: document link list component

---
+++ apps/examples/app/examples/link-list/demo.tsx
@@ -0,0 +1,28 @@
+import { Link, LinkList } from "@postenbring/hedwig-react";
+
+function Example() {
+  return (
+    <LinkList>
+      <li>
+        <Link href="https://hedwig.posten.no">Link somewhere</Link>
+      </li>
+      <li>
+        <Link href="https://hedwig.posten.no" target="_blank">
+          Link somewhere
+        </Link>
+      </li>
+      <li>
+        <Link href="https://hedwig.posten.no" target="_self">
+          Link somewhere
+        </Link>
+      </li>
+    </LinkList>
+  );
+}
+
+export default Example;
+
+import type { ExampleConfig } from "..";
+export const config: ExampleConfig = {
+  index: 0,
+};
---
+++ apps/examples/app/examples/link-list/using-a-tags.tsx
@@ -0,0 +1,37 @@
+import { LinkList } from "@postenbring/hedwig-react";
+
+function Example() {
+  return (
+    <LinkList>
+      <li>
+        <a
+          target="_blank"
+          href="https://www.skatteetaten.no/person/avgifter/kjop-fra-utlandet/"
+          rel="noreferrer"
+        >
+          Skatteetatens sider om kjøp fra utlandet
+        </a>
+      </li>
+      <li>
+        <a target="_blank" href="https://www.toll.no/no/netthandel/" rel="noreferrer">
+          Tolletatens netthandelssider
+        </a>
+      </li>
+      <li>
+        <a target="_blank" href="/fortolling/motta-fra-utlandet">
+          Fortolling av sendinger fra utlandet
+        </a>
+      </li>
+      <li>
+        <a href="/import">Kvittering for betaling av avgifter</a>
+      </li>
+    </LinkList>
+  );
+}
+
+export default Example;
+
+import type { ExampleConfig } from "..";
+export const config: ExampleConfig = {
+  description: "Unstyled <code>a</code> tags can be passed to the <code>LinkList</code> component.",
+};
