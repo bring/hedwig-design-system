388761560fb2a91655496f8f58131b4edd69ea34
Julian Jark <julian.jark@posten.no>
2024-04-21 23:13:18 +0200

    :fire: remove deprecated fill value `outlined` in favour of `outline`

---
+++ .changeset/three-parents-fly.md
@@ -0,0 +1,5 @@
+---
+"@postenbring/hedwig-react": patch
+---
+
+:fire: remove deprecated fill value `outlined` in favour of `outline`
--- packages/react/src/button/button.tsx
+++ packages/react/src/button/button.tsx
@@ -2,11 +2,16 @@ import { forwardRef } from "react";
 import { clsx } from "@postenbring/hedwig-css/typed-classname";
 import type { OverridableComponent } from "../utils";
 
-interface ButtonPropsInternal extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   /**
    * The height, font size and padding of the button
    */
   size?: "small" | "medium" | "large";
+
+  /**
+   * The background fill of the button
+   */
+  fill?: "contained" | "outline";
 
   /**
    * Make the button use 100% width available.
@@ -21,22 +26,6 @@ interface ButtonPropsInternal extends React.ButtonHTMLAttributes<HTMLButtonEleme
 
   children?: React.ReactNode;
 }
-
-export type ButtonProps = ButtonPropsInternal &
-  (
-    | {
-        /**
-         * The background fill of the button
-         */
-        fill?: "contained" | "outline";
-      }
-    | {
-        /**
-         * @deprecated use `fill='outline'` instead
-         */
-        fill?: "outlined";
-      }
-  );
 
 export const BaseButton: OverridableComponent<
   ButtonProps & { variant: "primary" | "secondary" },
@@ -56,12 +45,6 @@ export const BaseButton: OverridableComponent<
     },
     ref,
   ) => {
-    // TODO: Remove deprecated fill value name at some point
-    // Deprecated at 10. march 2024
-    if (fill === "outlined") {
-      // eslint-disable-next-line no-console -- Deprecation warning
-      console.warn("The `fill` value `outlined` is deprecated, use `fill='outline'` instead");
-    }
     return (
       <Component
         className={clsx(
@@ -69,7 +52,7 @@ export const BaseButton: OverridableComponent<
           `hds-button--${size}`,
           {
             [`hds-button--${variant}`]: fill === "contained",
-            [`hds-button--outline-${variant}`]: fill === "outline" || fill === "outlined",
+            [`hds-button--outline-${variant}`]: fill === "outline",
             "hds-button--full": fullWidth === true,
             "hds-button--mobile-full": fullWidth === "mobile",
             "hds-button--icon-only": icon && !children,
