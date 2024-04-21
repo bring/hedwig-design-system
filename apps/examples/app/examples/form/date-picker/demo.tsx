e937917358532796b684f307fb4eecc663745432
Julian Jark <julian.jark@posten.no>
2024-04-21 22:25:47 +0200

    :memo: date-picker example

---
+++ apps/examples/app/examples/form/date-picker/demo.tsx
@@ -0,0 +1,20 @@
+import { DatePicker } from "@postenbring/hedwig-react";
+
+function Example() {
+  return (
+    <DatePicker
+      label="Choose a date"
+      min="2024-04-05"
+      max="2026-04-04"
+      calendarButtonTitle="Open calendar"
+      style={{ minWidth: "200px" }}
+    />
+  );
+}
+
+export default Example;
+
+import type { ExampleConfig } from "../..";
+export const config: ExampleConfig = {
+  index: 0,
+};
