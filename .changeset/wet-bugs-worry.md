5da92eb7e3e72da2fe00ed2e0c5f895b4e662b85
Julian Jark <julian.jark@posten.no>
2024-04-25 17:35:12 +0200

    :wheelchair: increase contrast on input focused state

---
+++ .changeset/wet-bugs-worry.md
@@ -0,0 +1,5 @@
+---
+"@postenbring/hedwig-css": patch
+---
+
+:wheelchair: increase contrast on input focused state
--- apps/examples/app/examples/form/input/demo.tsx
+++ apps/examples/app/examples/form/input/demo.tsx
@@ -1,12 +1,47 @@
-import { Input } from "@postenbring/hedwig-react";
+import {
+  Container,
+  Input,
+  PrimaryButton,
+  Select,
+  Textarea,
+  VStack,
+} from "@postenbring/hedwig-react";
 
 function Example() {
-  return <Input label="Label" />;
+  function onSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
+  return (
+    <Container variant="slim">
+      <form onSubmit={onSubmit} style={{ padding: "var(--hds-spacing-20-24) 0" }}>
+        <VStack gap="16-20">
+          <Input label="Name" name="name" />
+          <Input label="Email" type="email" name="name" />
+          <Input
+            label="Password"
+            type="password"
+            name="password"
+            placeholder="At least 6 characters"
+          />
+          <Textarea label="Profile bio (optional)" />
+          <Select label="Preferred animal">
+            <option disabled selected value="">
+              Choose an animal
+            </option>
+            <option value="dog">Dog</option>
+            <option value="cat">Cat</option>
+            <option value="other">Other</option>
+          </Select>
+          <PrimaryButton>Submit</PrimaryButton>
+        </VStack>
+      </form>
+    </Container>
+  );
 }
 
 export default Example;
 
 import type { ExampleConfig } from "../..";
 export const config: ExampleConfig = {
-  layout: "centered-fullwidth",
+  layout: "none",
 };
