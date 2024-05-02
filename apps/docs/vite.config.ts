76a8deead08f3af4c513ae44ef50422b07ab3a24
Julian Jark <julian.jark@posten.no>
2024-05-02 22:47:43 +0200

    :wrench: update storybook vite.config.ts

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -6,6 +6,10 @@ export default defineConfig({
   resolve: {
     alias: [
       {
+        find: "@postenbring/hedwig-react/dist/index-no-css",
+        replacement: new URL("../../packages/react/src/index-no-css.tsx", import.meta.url).pathname,
+      },
+      {
         find: "@postenbring/hedwig-react",
         replacement: new URL("../../packages/react/src", import.meta.url).pathname,
       },
