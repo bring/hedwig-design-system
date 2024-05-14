82a533faca90cd45c966eb6c3f1fc4213b9e4914
Julian Jark <julian.jark@posten.no>
2024-05-14 23:22:36 +0200

    :children_crossing: warning-banner: make the clickable area bigger and set a max-width for the text

--- packages/react/src/warning-banner/warning-banner.tsx
+++ packages/react/src/warning-banner/warning-banner.tsx
@@ -1,7 +1,6 @@
 import type { ReactNode } from "react";
-import React, { forwardRef, useState } from "react";
+import React, { forwardRef, useId, useState } from "react";
 import { clsx } from "@postenbring/hedwig-css/typed-classname";
-import { Box } from "../box";
 
 export interface WarningBannerProps extends Omit<React.HTMLAttributes<HTMLDivElement>, "title"> {
   title: ReactNode;
@@ -10,36 +9,47 @@ export interface WarningBannerProps extends Omit<React.HTMLAttributes<HTMLDivEle
 
 export const WarningBanner = forwardRef<HTMLDivElement, WarningBannerProps>(
   ({ title, description, className, ...rest }, ref) => {
+    const descriptionId = useId();
     const expandable = !!description;
     return (
-      <Box {...rest} className={clsx("hds-warning-banner", className as undefined)} ref={ref}>
-        <WarningBannerTitle variant={expandable ? "expandable" : "default"}>
+      <div {...rest} className={clsx("hds-warning-banner", className as undefined)} ref={ref}>
+        <WarningBannerTitle
+          variant={expandable ? "expandable" : "default"}
+          descriptionId={descriptionId}
+        >
           {title}
         </WarningBannerTitle>
-        {expandable ? <WarningBannerDescription>{description}</WarningBannerDescription> : null}
-      </Box>
+        {expandable ? (
+          <WarningBannerDescription id={descriptionId}>{description}</WarningBannerDescription>
+        ) : null}
+      </div>
     );
   },
 );
 WarningBanner.displayName = "WarningBanner";
 
 type WarningBannerTitleProps =
-  | ({ variant: "expandable" } & React.ButtonHTMLAttributes<HTMLButtonElement>)
-  | ({ variant: "default" } & React.HTMLAttributes<HTMLParagraphElement>);
+  | ({
+      variant: "expandable";
+      descriptionId: string;
+    } & React.ButtonHTMLAttributes<HTMLButtonElement>)
+  | ({ variant: "default"; descriptionId?: string } & React.HTMLAttributes<HTMLParagraphElement>);
 
 const WarningBannerTitle = forwardRef<
   HTMLButtonElement | HTMLParagraphElement,
   WarningBannerTitleProps
->(({ variant, children, className, ...rest }, ref) => {
+>(({ variant, descriptionId, children, className, ...rest }, ref) => {
   const [open, setOpen] = useState<boolean>(false);
   if (variant === "expandable") {
     return (
       <button
         {...(rest as React.ButtonHTMLAttributes<HTMLButtonElement>)}
+        aria-expanded={open}
+        aria-controls={descriptionId}
+        data-state={open ? "open" : "closed"}
         className={clsx(
           "hds-warning-banner__title",
           "hds-warning-banner__title-trigger",
-          { "hds-warning-banner--closed": !open },
           className as undefined,
         )}
         onClick={() => {
@@ -48,7 +58,7 @@ const WarningBannerTitle = forwardRef<
         ref={ref as React.Ref<HTMLButtonElement>}
         type="button"
       >
-        <span>{children}</span>
+        <div>{children}</div>
       </button>
     );
   }
@@ -64,11 +74,12 @@ const WarningBannerTitle = forwardRef<
 });
 WarningBannerTitle.displayName = "WarningBannerTitle";
 
-type WarningBannerDescriptionProps = React.HTMLAttributes<HTMLParagraphElement>;
+type WarningBannerDescriptionProps = { id: string } & React.HTMLAttributes<HTMLParagraphElement>;
 const WarningBannerDescription = forwardRef<HTMLParagraphElement, WarningBannerDescriptionProps>(
-  ({ className, ...rest }, ref) => {
+  ({ className, id, ...rest }, ref) => {
     return (
       <p
+        id={id}
         className={clsx("hds-warning-banner__description", className as undefined)}
         ref={ref}
         {...rest}
