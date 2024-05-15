55506554c701222c10eddea02aa44dac33bb8b0e
Julian Jark <julian.jark@posten.no>
2024-05-15 10:29:37 +0200

    :memo: fix example

--- apps/examples/app/examples/badge/warning.tsx
+++ apps/examples/app/examples/badge/warning.tsx
@@ -4,7 +4,9 @@ function Example() {
   return (
     <HStack gap="8" align="end">
       <Badge variant="warning">Warning</Badge>
-      <Badge size="smaller">Warning</Badge>
+      <Badge variant="warning" size="smaller">
+        Warning
+      </Badge>
     </HStack>
   );
 }
