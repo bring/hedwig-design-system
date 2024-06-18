c5e1d00fa7487d12c735a831864f01a66104a389
Julian Jark <julian.jark@posten.no>
2024-06-18 21:20:47 +0200

    :truck:

---
+++ apps/examples/app/routes/_storefront.storefront.$/mdx-components.tsx
@@ -0,0 +1,42 @@
+import { Examples } from "../../components/component-examples";
+import { FigmaEmbed, FigmaPreviews } from "../../components/figma";
+import type { TemplateNames } from "../../../tina/templates";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const MDXComponents: Record<TemplateNames, (props: any) => JSX.Element> = {
+  Examples: (props: {
+    componentName: string;
+    exampleName?: string;
+    showCodeByDefault?: boolean;
+  }) => {
+    return (
+      <div className="hds-mt-24-32">
+        <Examples
+          componentName={props.componentName}
+          exampleName={props.exampleName}
+          showCodeByDefault={props.showCodeByDefault ?? false}
+        />
+      </div>
+    );
+  },
+  FigmaPreviews: (props: { urls: string[] }) => {
+    const urls = props.urls;
+    return (
+      <div className="hds-mt-24-32">
+        <FigmaPreviews urls={urls} />
+      </div>
+    );
+  },
+  FigmaEmbed: (props: { url: string; hideBottomBar?: boolean }) => {
+    return (
+      <div className="hds-mt-24-32">
+        <FigmaEmbed
+          urlToEmbed={props.url}
+          height={1200}
+          width="80%"
+          hideBottomBar={props.hideBottomBar}
+        />
+      </div>
+    );
+  },
+};
