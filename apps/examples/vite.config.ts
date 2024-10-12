48fc95195601c0aca46fbdb6631de00be9085387
Julian Jark <julian.jark@posten.no>
2024-10-12 19:59:26 +0200

    :wrench: examples: enable remix/react-router future features

--- apps/examples/vite.config.ts
+++ apps/examples/vite.config.ts
@@ -38,6 +38,14 @@ export default defineConfig({
         });
         return routes;
       },
+
+      future: {
+        v3_singleFetch: true,
+        v3_lazyRouteDiscovery: true,
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
     }),
   ],
   resolve: {
