fc4e63d711ecb7755b4508d4725980fb9f7951ea
Dag Øyvind Tornes <dag.oyvind.tornes@bring.com>
2024-10-15 14:28:03 +0200

    Use anchors instead of refs to focus on input with error

--- apps/examples/app/examples/form/error-summary/demo.tsx
+++ apps/examples/app/examples/form/error-summary/demo.tsx
@@ -1,19 +1,16 @@
 import { Input, ErrorSummary, ErrorSummaryError } from "@postenbring/hedwig-react";
-import { useRef } from "react";
 import type { ExampleConfig } from "../..";
 
 function Example() {
-  const ref1 = useRef<HTMLInputElement>(null);
-  const ref2 = useRef<HTMLInputElement>(null);
   const errors: ErrorSummaryError[] = [
-    { message: "Feil i input", ref: ref1 },
-    { message: "Feil i input", ref: ref2 },
+    { message: "Feil i input", anchor: "#test1" },
+    { message: "Feil i input", anchor: "#test2" },
   ];
   return (
     <div>
       <ErrorSummary heading={"Test feiloppsummering"} errors={errors} />
-      <Input label="Test 1" name="test1" ref={ref1} errorMessage={errors[0].message} />
-      <Input label="Test 2" name="test2" ref={ref2} errorMessage={errors[1].message} />
+      <Input id="test1" label="Test 1" name="test1" errorMessage={errors[0].message} />
+      <Input id="test2" label="Test 2" name="test2" errorMessage={errors[1].message} />
     </div>
   );
 }
--- packages/react/src/form/error-summary/error-summary.tsx
+++ packages/react/src/form/error-summary/error-summary.tsx
@@ -1,22 +1,20 @@
-import { type RefObject, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { Message } from "../../message";
 import { UnorderedList } from "../../list";
 import { Link } from "../../link";
 
 export interface ErrorSummaryError {
   message: string;
-  ref?: RefObject<HTMLInputElement>; // TODO: Is input-element okay here? Probably, since error summaries usually
+  anchor?: string;
 }
 
 export interface ErrorSummaryProps {
   heading: string;
   errors: ErrorSummaryError[];
   autoFocus?: boolean;
-  id?: string;
-  className?: string;
 }
 
-export function ErrorSummary({ heading, autoFocus, errors }: ErrorSummaryProps) {
+export function ErrorSummary({ heading, errors, autoFocus = false }: ErrorSummaryProps) {
   // Automatically focus on this element when it enters the DOM
   const headingRef = useRef<HTMLParagraphElement>(null);
   useEffect(() => {
@@ -32,13 +30,7 @@ export function ErrorSummary({ heading, autoFocus, errors }: ErrorSummaryProps) 
         <UnorderedList size="small">
           {errors.map((error) => (
             <li key={error.message}>
-              <Link
-                size="small"
-                onClick={(event) => {
-                  event.preventDefault();
-                  error.ref?.current?.focus();
-                }}
-              >
+              <Link size="small" href={error.anchor}>
                 {error.message}
               </Link>
             </li>
