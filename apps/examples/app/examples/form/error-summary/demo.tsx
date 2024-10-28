729c31804a23b0471c42abb965d2c0973cd63ccc
Julian Jark <julian.jark@posten.no>
2024-10-28 22:42:23 +0100

    updates

--- apps/examples/app/examples/form/error-summary/demo.tsx
+++ apps/examples/app/examples/form/error-summary/demo.tsx
@@ -1,16 +1,73 @@
 import "@postenbring/hedwig-css";
-import { Input, ErrorSummary, ErrorSummaryError, VStack } from "@postenbring/hedwig-react";
+import {
+  Input,
+  ErrorSummary,
+  VStack,
+  Button,
+  RadioButton,
+  RadioGroup,
+} from "@postenbring/hedwig-react";
 
 function Example() {
-  const errors: ErrorSummaryError[] = [
-    { message: "Feil i input", anchor: "#test1" },
-    { message: "Feil i input", anchor: "#test2" },
-  ];
+  const errorsById: Record<string, string | undefined> = {
+    mobilenumber: "Mobile number is required",
+    firstname: "First name is required",
+    "favorite-food": "Favorite food is required",
+  };
+
   return (
-    <VStack gap="12-16">
-      <ErrorSummary heading={"Test feiloppsummering"} errors={errors} />
-      <Input id="test1" label="Test 1" name="test1" errorMessage={errors[0].message} />
-      <Input id="test2" label="Test 2" name="test2" errorMessage={errors[1].message} />
+    <VStack gap="24" asChild>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <Input
+          label="Mobile number"
+          id="mobilenumber"
+          name="mobilenumber"
+          errorMessage={errorsById["mobilenumber"]}
+        />
+        <Input
+          label="Email"
+          id="email"
+          name="email"
+          type="email"
+          errorMessage={errorsById["email"]}
+        />
+        <Input
+          label="First name"
+          id="firstname"
+          name="firstname"
+          errorMessage={errorsById["firstname"]}
+        />
+        <Input label="Surname" id="surname" name="surname" errorMessage={errorsById["surname"]} />
+
+        <RadioGroup
+          legend="Favorite food"
+          name="favorite-food"
+          errorMessage={errorsById["favorite-food"]}
+        >
+          <RadioButton value="Pizza" id="favorite-food">
+            🍕 Pizza
+          </RadioButton>
+          <RadioButton value="Burger">🍔 Burger</RadioButton>
+          <RadioButton value="Sushi">🍱 Sushi</RadioButton>
+        </RadioGroup>
+
+        <ErrorSummary>
+          <ErrorSummary.Heading>
+            To continue please correct the following issues
+          </ErrorSummary.Heading>
+          <ErrorSummary.List>
+            {Object.entries(errorsById).map(([id, message]) => (
+              <ErrorSummary.Item key={id} href={`#${id}`}>
+                {message}
+              </ErrorSummary.Item>
+            ))}
+          </ErrorSummary.List>
+        </ErrorSummary>
+
+        <Button type="submit" fullWidth="mobile" style={{ alignSelf: "start" }}>
+          Fortsett
+        </Button>
+      </form>
     </VStack>
   );
 }
@@ -20,5 +77,5 @@ export default Example;
 import type { ExampleConfig } from "../..";
 export const config: ExampleConfig = {
   index: 0,
-  layout: "centered-fullwidth",
+  layout: "centered-vertical-padding",
 };
--- packages/react/src/form/error-summary/error-summary.tsx
+++ packages/react/src/form/error-summary/error-summary.tsx
@@ -1,18 +1,70 @@
-import { forwardRef, useEffect, useRef } from "react";
-import { Message } from "../../message";
-import { UnorderedList } from "../../list";
+import { forwardRef } from "react";
+import { Message, type MessageProps } from "../../message";
+import { UnorderedList, type ListProps } from "../../list";
 import { Link } from "../../link";
+import { focusWithLegendOrLabelInViewport } from "./focus";
+
+type ErrorSummaryHeadingProps = React.HTMLAttributes<HTMLParagraphElement>;
+export const ErrorSummaryHeading = forwardRef<HTMLParagraphElement, ErrorSummaryHeadingProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <Message.Title ref={ref} {...rest}>
+        {children}
+      </Message.Title>
+    );
+  },
+);
+ErrorSummaryHeading.displayName = "ErrorSummary.Heading";
+
+interface ErrorSummaryListProps extends ListProps {
+  /**
+   * Sets the size of the items (font)
+   *
+   * @default "small"
+   */
+  size?: ListProps["size"];
+}
+export const ErrorSummaryList = forwardRef<HTMLUListElement, ErrorSummaryListProps>(
+  ({ children, size = "small", ...rest }, ref) => {
+    return (
+      <Message.Description asChild>
+        <UnorderedList size={size} ref={ref} {...rest}>
+          {children}
+        </UnorderedList>
+      </Message.Description>
+    );
+  },
+);
+ErrorSummaryList.displayName = "ErrorSummary.List";
 
 interface ErrorSummaryItemProps extends React.HTMLAttributes<HTMLLIElement> {
-  message: React.ReactNode;
+  /**
+   * A hash link to the element that the error message refers to
+   *
+   * Must start with "#" as it's passed to the `href` attribute of an anchor element
+   *
+   * @example "#email"
+   */
   href: `#${string}`;
+
+  /**
+   * Extra props to pass to the link element
+   */
+  linkProps?: React.AnchorHTMLAttributes<HTMLAnchorElement>;
 }
 export const ErrorSummaryItem = forwardRef<HTMLLIElement, ErrorSummaryItemProps>(
-  ({ message, href }, ref) => {
+  ({ children, href, linkProps, ...rest }, ref) => {
+    function onClick(e: React.MouseEvent<HTMLAnchorElement>) {
+      linkProps?.onClick?.(e);
+      if (focusWithLegendOrLabelInViewport(href.replace("#", ""))) {
+        e.preventDefault();
+      }
+    }
+
     return (
-      <li ref={ref}>
-        <Link size="small" href={href}>
-          {message}
+      <li ref={ref} {...rest}>
+        <Link size="small" href={href} {...linkProps} onClick={onClick}>
+          {children}
         </Link>
       </li>
     );
@@ -20,34 +72,16 @@ export const ErrorSummaryItem = forwardRef<HTMLLIElement, ErrorSummaryItemProps>
 );
 ErrorSummaryItem.displayName = "ErrorSummary.Item";
 
-export interface ErrorSummaryError {
-  message: string;
-  anchor: string;
-}
-
-export interface ErrorSummaryProps extends React.HTMLAttributes<HTMLDivElement> {
-  heading: string;
-  errors: ErrorSummaryError[];
+export interface ErrorSummaryProps
+  extends Omit<MessageProps, "variant" | "icon" | "iconClassName"> {
   autoFocus?: boolean;
 }
 
 export const ErrorSummary = forwardRef<HTMLDivElement, ErrorSummaryProps>(
-  ({ children, heading, autoFocus = false, ...rest }, ref) => {
-    // Automatically focus on this element when it enters the DOM
-    const headingRef = useRef<HTMLParagraphElement>(null);
-    useEffect(() => {
-      if (autoFocus) {
-        headingRef.current?.focus();
-      }
-      // eslint-disable-next-line react-hooks/exhaustive-deps -- autoFocus should only run once
-    }, []);
-
+  ({ children, ...rest }, ref) => {
     return (
       <Message variant="warning" ref={ref} {...rest}>
-        <Message.Title ref={headingRef}>{heading}</Message.Title>
-        <Message.Description>
-          <UnorderedList size="small">{children}</UnorderedList>
-        </Message.Description>
+        {children}
       </Message>
     );
   },
@@ -55,6 +89,10 @@ export const ErrorSummary = forwardRef<HTMLDivElement, ErrorSummaryProps>(
 ErrorSummary.displayName = "ErrorSummary";
 
 type ErrorSummaryType = ReturnType<typeof forwardRef<HTMLDivElement, ErrorSummaryProps>> & {
+  Heading: typeof ErrorSummaryHeading;
+  List: typeof ErrorSummaryList;
   Item: typeof ErrorSummaryItem;
 };
+ErrorSummary.Heading = ErrorSummaryHeading;
+ErrorSummary.List = ErrorSummaryList;
 ErrorSummary.Item = ErrorSummaryItem;
