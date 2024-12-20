6967033c89123d0361a572ab71d9eebfa0d59392
Viggo Normann <v@iggo.no>
2024-12-20 11:43:50 +0100

    Add more examples for Miniature cards

--- apps/examples/app/examples/card/miniature.tsx
+++ apps/examples/app/examples/card/miniature.tsx
@@ -4,68 +4,206 @@ import customerParcel from "../../assets/customer-parcel.svg";
 
 function Example() {
   return (
-    <Container>
-      <div
-        style={{
-          margin: "var(--hds-spacing-20)",
-          display: "grid",
-          gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
-          gap: "var(--hds-spacing-24-32)",
-        }}
-      >
-        <Card variant="miniature">
-          <Card.Media>
-            <Card.MediaImg alt="posten-bring" src={customerParcel} />
-          </Card.Media>
-          <Card.Body>
-            <Card.BodyHeader as="h2">
-              <Card.BodyHeaderTitle>Cool article</Card.BodyHeaderTitle>
-            </Card.BodyHeader>
-            <Card.BodyDescription>
-              If you have an address label for return, you can place it on the middle of the parcel.
-            </Card.BodyDescription>
-            <Card.BodyAction>
-              <Link href="https://www.posten.no" variant="solid" target="_blank">
-                <Card.BodyActionArrow direction="up-right" />
-                Only link
-              </Link>
-            </Card.BodyAction>
-          </Card.Body>
-        </Card>
-        <Card variant="miniature">
-          <Card.Media>
-            <Card.MediaImg alt="posten-bring" src={customerParcel} />
-          </Card.Media>
-          <Card.Body>
-            <Card.BodyHeader as="h2">
-              <Card.BodyHeaderTitle>Another article</Card.BodyHeaderTitle>
-            </Card.BodyHeader>
-            <Card.BodyDescription>
-              If you have an address label for return, you can place it on the middle of the parcel.
-            </Card.BodyDescription>
-            <Card.BodyAction asChild>
-              <Link href="https://www.posten.no" variant="solid" target="_blank">
-                <Card.BodyActionArrow />
-                Only link
-              </Link>
-            </Card.BodyAction>
-          </Card.Body>
-        </Card>
-        <Card variant="miniature">
-          <Card.Media>
-            <Card.MediaImg alt="posten-bring" src={customerParcel} />
-          </Card.Media>
-          <Card.Body>
-            <Card.BodyAction asChild>
-              <Link href="https://www.posten.no" variant="solid" target="_blank">
-                <Card.BodyActionArrow />
-                Only link
-              </Link>
-            </Card.BodyAction>
-          </Card.Body>
-        </Card>
-      </div>
-    </Container>
+    <>
+      <Container>
+        <h2>Color - Lighter Brand - Default</h2>
+        <div
+          style={{
+            marginTop: "var(--hds-spacing-20)",
+            marginBottom: "var(--hds-spacing-20)",
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
+            gap: "var(--hds-spacing-24-32)",
+          }}
+        >
+          <Card variant="miniature" color="lighter-brand">
+            <Card.Media>
+              <Card.MediaImg alt="posten-bring" src={customerParcel} />
+            </Card.Media>
+            <Card.Body>
+              <Card.BodyHeader as="h2">
+                <Card.BodyHeaderTitle>Cool article</Card.BodyHeaderTitle>
+              </Card.BodyHeader>
+              <Card.BodyDescription>
+                If you have an address label for return, you can place it on the middle of the
+                parcel.
+              </Card.BodyDescription>
+              <Card.BodyAction>
+                <Link href="https://www.posten.no" variant="solid" target="_blank">
+                  <Card.BodyActionArrow direction="up-right" />
+                  Only link
+                </Link>
+              </Card.BodyAction>
+            </Card.Body>
+          </Card>
+          <Card variant="miniature" color="lighter-brand">
+            <Card.Media>
+              <Card.MediaImg alt="posten-bring" src={customerParcel} />
+            </Card.Media>
+            <Card.Body>
+              <Card.BodyHeader as="h2">
+                <Card.BodyHeaderTitle>Another article</Card.BodyHeaderTitle>
+              </Card.BodyHeader>
+              <Card.BodyDescription>
+                If you have an address label for return, you can place it on the middle of the
+                parcel.
+              </Card.BodyDescription>
+              <Card.BodyAction asChild>
+                <Link href="https://www.posten.no" variant="solid" target="_blank">
+                  <Card.BodyActionArrow />
+                  Only link
+                </Link>
+              </Card.BodyAction>
+            </Card.Body>
+          </Card>
+          <Card variant="miniature" color="lighter-brand">
+            <Card.Media>
+              <Card.MediaImg alt="posten-bring" src={customerParcel} />
+            </Card.Media>
+            <Card.Body>
+              <Card.BodyAction asChild>
+                <Link href="https://www.posten.no" variant="solid" target="_blank">
+                  <Card.BodyActionArrow />
+                  Only link
+                </Link>
+              </Card.BodyAction>
+            </Card.Body>
+          </Card>
+        </div>
+      </Container>
+      <Container>
+        <h2>Color - Light Grey Fill</h2>
+        <div
+          style={{
+            marginTop: "var(--hds-spacing-20)",
+            marginBottom: "var(--hds-spacing-20)",
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
+            gap: "var(--hds-spacing-24-32)",
+          }}
+        >
+          <Card variant="miniature" color="light-grey-fill">
+            <Card.Media>
+              <Card.MediaImg alt="posten-bring" src={customerParcel} />
+            </Card.Media>
+            <Card.Body>
+              <Card.BodyHeader as="h2">
+                <Card.BodyHeaderTitle>Cool article</Card.BodyHeaderTitle>
+              </Card.BodyHeader>
+              <Card.BodyDescription>
+                If you have an address label for return, you can place it on the middle of the
+                parcel.
+              </Card.BodyDescription>
+              <Card.BodyAction>
+                <Link href="https://www.posten.no" variant="solid" target="_blank">
+                  <Card.BodyActionArrow direction="up-right" />
+                  Only link
+                </Link>
+              </Card.BodyAction>
+            </Card.Body>
+          </Card>
+          <Card variant="miniature" color="light-grey-fill">
+            <Card.Media>
+              <Card.MediaImg alt="posten-bring" src={customerParcel} />
+            </Card.Media>
+            <Card.Body>
+              <Card.BodyHeader as="h2">
+                <Card.BodyHeaderTitle>Another article</Card.BodyHeaderTitle>
+              </Card.BodyHeader>
+              <Card.BodyDescription>
+                If you have an address label for return, you can place it on the middle of the
+                parcel.
+              </Card.BodyDescription>
+              <Card.BodyAction asChild>
+                <Link href="https://www.posten.no" variant="solid" target="_blank">
+                  <Card.BodyActionArrow />
+                  Only link
+                </Link>
+              </Card.BodyAction>
+            </Card.Body>
+          </Card>
+          <Card variant="miniature" color="light-grey-fill">
+            <Card.Media>
+              <Card.MediaImg alt="posten-bring" src={customerParcel} />
+            </Card.Media>
+            <Card.Body>
+              <Card.BodyAction asChild>
+                <Link href="https://www.posten.no" variant="solid" target="_blank">
+                  <Card.BodyActionArrow />
+                  Only link
+                </Link>
+              </Card.BodyAction>
+            </Card.Body>
+          </Card>
+        </div>
+      </Container>
+      <Container>
+        <h2>Color - White</h2>
+        <div
+          style={{
+            marginTop: "var(--hds-spacing-20)",
+            marginBottom: "var(--hds-spacing-20)",
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
+            gap: "var(--hds-spacing-24-32)",
+          }}
+        >
+          <Card variant="miniature" color="white">
+            <Card.Media>
+              <Card.MediaImg alt="posten-bring" src={customerParcel} />
+            </Card.Media>
+            <Card.Body>
+              <Card.BodyHeader as="h2">
+                <Card.BodyHeaderTitle>Cool article</Card.BodyHeaderTitle>
+              </Card.BodyHeader>
+              <Card.BodyDescription>
+                If you have an address label for return, you can place it on the middle of the
+                parcel.
+              </Card.BodyDescription>
+              <Card.BodyAction>
+                <Link href="https://www.posten.no" variant="solid" target="_blank">
+                  <Card.BodyActionArrow direction="up-right" />
+                  Only link
+                </Link>
+              </Card.BodyAction>
+            </Card.Body>
+          </Card>
+          <Card variant="miniature" color="white">
+            <Card.Media>
+              <Card.MediaImg alt="posten-bring" src={customerParcel} />
+            </Card.Media>
+            <Card.Body>
+              <Card.BodyHeader as="h2">
+                <Card.BodyHeaderTitle>Another article</Card.BodyHeaderTitle>
+              </Card.BodyHeader>
+              <Card.BodyDescription>
+                If you have an address label for return, you can place it on the middle of the
+                parcel.
+              </Card.BodyDescription>
+              <Card.BodyAction asChild>
+                <Link href="https://www.posten.no" variant="solid" target="_blank">
+                  <Card.BodyActionArrow />
+                  Only link
+                </Link>
+              </Card.BodyAction>
+            </Card.Body>
+          </Card>
+          <Card variant="miniature" color="white">
+            <Card.Media>
+              <Card.MediaImg alt="posten-bring" src={customerParcel} />
+            </Card.Media>
+            <Card.Body>
+              <Card.BodyAction asChild>
+                <Link href="https://www.posten.no" variant="solid" target="_blank">
+                  <Card.BodyActionArrow />
+                  Only link
+                </Link>
+              </Card.BodyAction>
+            </Card.Body>
+          </Card>
+        </div>
+      </Container>
+    </>
   );
 }
 
