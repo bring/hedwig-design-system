3161d2464bcb8c6e40aa909a8a8bc3ef8191916b
Viggo Normann <v@iggo.no>
2025-01-14 14:38:52 +0100

    Update examples for the Slim cards to be more relevant

--- apps/examples/app/examples/card/slim.tsx
+++ apps/examples/app/examples/card/slim.tsx
@@ -16,81 +16,88 @@ function Example() {
         >
           <Card variant="slim" color="lighter-brand">
             <Card.Media>
-              <Card.MediaImg alt="posten-bring" src={postenBringImage} />
+              <Card.MediaImg
+                alt="Posten delivery van with Bring cargo truck in the background"
+                src={postenBringImage}
+              />
             </Card.Media>
             <Card.Body>
               <Card.BodyHeader as="h3">
-                <Card.BodyHeaderOverline>Color - Lighter Brand - Default</Card.BodyHeaderOverline>
-                <Card.BodyHeaderTitle>Article 1</Card.BodyHeaderTitle>
+                <Card.BodyHeaderOverline>Services</Card.BodyHeaderOverline>
+                <Card.BodyHeaderTitle>Information about services</Card.BodyHeaderTitle>
               </Card.BodyHeader>
-              <Card.BodyDescription>
-                This is the default card variant. The link below is preceeded by an arrow.
-              </Card.BodyDescription>
+              <Card.BodyDescription>Find the right service and order</Card.BodyDescription>
               <Card.BodyAction>
                 <Link href="#article-1" variant="solid">
                   <Card.BodyActionArrow />
-                  Read more
+                  Find a service
                 </Link>
               </Card.BodyAction>
             </Card.Body>
           </Card>
           <Card variant="slim" color="lighter-brand">
             <Card.Media>
-              <Card.MediaImg alt="posten-bring" src={postenBringImage} />
+              <Card.MediaImg
+                alt="Posten delivery van with Bring cargo truck in the background"
+                src={postenBringImage}
+              />
             </Card.Media>
             <Card.Body>
               <Card.BodyHeader as="h3">
-                <Card.BodyHeaderOverline>Color - Lighter Brand - Default</Card.BodyHeaderOverline>
-                <Card.BodyHeaderTitle>Article 2 </Card.BodyHeaderTitle>
+                <Card.BodyHeaderOverline>Customer service</Card.BodyHeaderOverline>
+                <Card.BodyHeaderTitle>Contact Customer service</Card.BodyHeaderTitle>
               </Card.BodyHeader>
               <Card.BodyDescription>
-                The slim cards are a collection of items. They will have the same height while on
-                the same row.
+                Contact our customer service for help with your questions
               </Card.BodyDescription>
               <Card.BodyAction>
                 <Card.BodyActionArrow />
                 <Link href="#article-2" variant="solid">
-                  Read more
+                  Get in touch
                 </Link>
               </Card.BodyAction>
             </Card.Body>
           </Card>
           <Card variant="slim" color="lighter-brand">
             <Card.Media>
-              <Card.MediaImg alt="posten-bring" src={postenBringImage} />
+              <Card.MediaImg
+                alt="Posten delivery van with Bring cargo truck in the background"
+                src={postenBringImage}
+              />
             </Card.Media>
             <Card.Body>
               <Card.BodyHeader as="h3">
-                <Card.BodyHeaderOverline>Color - Lighter Brand - Default</Card.BodyHeaderOverline>
-                <Card.BodyHeaderTitle>Article 3</Card.BodyHeaderTitle>
+                <Card.BodyHeaderOverline>Quote</Card.BodyHeaderOverline>
+                <Card.BodyHeaderTitle>Price enquiry</Card.BodyHeaderTitle>
               </Card.BodyHeader>
-              <Card.BodyDescription>
-                If there is a lot of contents in the card body, the media will be cropped to make
-                the cards look equally crowded. The links will be aligned at the bottom across the
-                cards in a row.
-              </Card.BodyDescription>
+              <Card.BodyDescription>Request a quote for your business</Card.BodyDescription>
               <Card.BodyAction>
                 <Link href="#article-3" variant="solid">
                   <Card.BodyActionArrow />
-                  Read more
+                  Request a quote
                 </Link>
               </Card.BodyAction>
             </Card.Body>
           </Card>
           <Card variant="slim" color="lighter-brand">
             <Card.Media>
-              <Card.MediaImg alt="posten-bring" src={postenBringImage} />
+              <Card.MediaImg
+                alt="Posten delivery van with Bring cargo truck in the background"
+                src={postenBringImage}
+              />
             </Card.Media>
             <Card.Body>
               <Card.BodyHeader as="h3">
-                <Card.BodyHeaderOverline>Color - Lighter Brand - Default</Card.BodyHeaderOverline>
-                <Card.BodyHeaderTitle>Article 4</Card.BodyHeaderTitle>
+                <Card.BodyHeaderOverline>Customs</Card.BodyHeaderOverline>
+                <Card.BodyHeaderTitle>Get familiar with customs</Card.BodyHeaderTitle>
               </Card.BodyHeader>
-              <Card.BodyDescription>A short card.</Card.BodyDescription>
+              <Card.BodyDescription>
+                Familiarise yourself with regulations relating to customs and fees
+              </Card.BodyDescription>
               <Card.BodyAction>
                 <Link href="#article-4" variant="solid">
                   <Card.BodyActionArrow />
-                  Read more
+                  Find out about customs
                 </Link>
               </Card.BodyAction>
             </Card.Body>
@@ -109,16 +116,14 @@ function Example() {
           <Card variant="slim" color="light-grey-fill">
             <Card.Body>
               <Card.BodyHeader as="h3">
-                <Card.BodyHeaderOverline>Color - Light Grey Fill</Card.BodyHeaderOverline>
-                <Card.BodyHeaderTitle>Article 1</Card.BodyHeaderTitle>
+                <Card.BodyHeaderOverline>Services</Card.BodyHeaderOverline>
+                <Card.BodyHeaderTitle>Information about services</Card.BodyHeaderTitle>
               </Card.BodyHeader>
-              <Card.BodyDescription>
-                This is the slim card without a media element.
-              </Card.BodyDescription>
+              <Card.BodyDescription>Find the right service and order</Card.BodyDescription>
               <Card.BodyAction>
                 <Link href="#article-1" variant="solid">
                   <Card.BodyActionArrow />
-                  Read more
+                  Find a service
                 </Link>
               </Card.BodyAction>
             </Card.Body>
@@ -126,17 +131,16 @@ function Example() {
           <Card variant="slim" color="light-grey-fill">
             <Card.Body>
               <Card.BodyHeader as="h3">
-                <Card.BodyHeaderOverline>Color - Light Grey Fill</Card.BodyHeaderOverline>
-                <Card.BodyHeaderTitle>Article 2 </Card.BodyHeaderTitle>
+                <Card.BodyHeaderOverline>Customer service</Card.BodyHeaderOverline>
+                <Card.BodyHeaderTitle>Contact Customer service</Card.BodyHeaderTitle>
               </Card.BodyHeader>
               <Card.BodyDescription>
-                The slim cards are a collection of items. They will have the same height while on
-                the same row.
+                Contact our customer service for help with your questions
               </Card.BodyDescription>
               <Card.BodyAction>
                 <Card.BodyActionArrow />
                 <Link href="#article-2" variant="solid">
-                  Read more
+                  Get in touch
                 </Link>
               </Card.BodyAction>
             </Card.Body>
@@ -144,17 +148,14 @@ function Example() {
           <Card variant="slim" color="light-grey-fill">
             <Card.Body>
               <Card.BodyHeader as="h3">
-                <Card.BodyHeaderOverline>Color - Light Grey Fill</Card.BodyHeaderOverline>
-                <Card.BodyHeaderTitle>Article 3</Card.BodyHeaderTitle>
+                <Card.BodyHeaderOverline>Quote</Card.BodyHeaderOverline>
+                <Card.BodyHeaderTitle>Price enquiry</Card.BodyHeaderTitle>
               </Card.BodyHeader>
-              <Card.BodyDescription>
-                If there is a lot of contents in the card body, the card will be taller. The links
-                will be aligned at the bottom across the cards in a row.
-              </Card.BodyDescription>
+              <Card.BodyDescription>Request a quote for your business</Card.BodyDescription>
               <Card.BodyAction>
                 <Link href="#article-3" variant="solid">
                   <Card.BodyActionArrow />
-                  Read more
+                  Request a quote
                 </Link>
               </Card.BodyAction>
             </Card.Body>
@@ -162,14 +163,16 @@ function Example() {
           <Card variant="slim" color="light-grey-fill">
             <Card.Body>
               <Card.BodyHeader as="h3">
-                <Card.BodyHeaderOverline>Color - Light Grey Fill</Card.BodyHeaderOverline>
-                <Card.BodyHeaderTitle>Article 4</Card.BodyHeaderTitle>
+                <Card.BodyHeaderOverline>Customs</Card.BodyHeaderOverline>
+                <Card.BodyHeaderTitle>Get familiar with customs</Card.BodyHeaderTitle>
               </Card.BodyHeader>
-              <Card.BodyDescription>A short card.</Card.BodyDescription>
+              <Card.BodyDescription>
+                Familiarise yourself with regulations relating to customs and fees
+              </Card.BodyDescription>
               <Card.BodyAction>
                 <Link href="#article-4" variant="solid">
                   <Card.BodyActionArrow />
-                  Read more
+                  Find out about customs
                 </Link>
               </Card.BodyAction>
             </Card.Body>
@@ -187,80 +190,88 @@ function Example() {
         >
           <Card variant="slim" color="white">
             <Card.Media>
-              <Card.MediaImg alt="posten-bring" src={postenBringImage} />
+              <Card.MediaImg
+                alt="Posten delivery van with Bring cargo truck in the background"
+                src={postenBringImage}
+              />
             </Card.Media>
             <Card.Body>
               <Card.BodyHeader as="h3">
-                <Card.BodyHeaderOverline>Color - White</Card.BodyHeaderOverline>
-                <Card.BodyHeaderTitle>Article 1</Card.BodyHeaderTitle>
+                <Card.BodyHeaderOverline>Services</Card.BodyHeaderOverline>
+                <Card.BodyHeaderTitle>Information about services</Card.BodyHeaderTitle>
               </Card.BodyHeader>
-              <Card.BodyDescription>
-                This is the slim card without a media element.
-              </Card.BodyDescription>
+              <Card.BodyDescription>Find the right service and order</Card.BodyDescription>
               <Card.BodyAction>
                 <Link href="#article-1" variant="solid">
                   <Card.BodyActionArrow />
-                  Read more
+                  Find a service
                 </Link>
               </Card.BodyAction>
             </Card.Body>
           </Card>
           <Card variant="slim" color="white">
             <Card.Media>
-              <Card.MediaImg alt="posten-bring" src={postenBringImage} />
+              <Card.MediaImg
+                alt="Posten delivery van with Bring cargo truck in the background"
+                src={postenBringImage}
+              />
             </Card.Media>
             <Card.Body>
               <Card.BodyHeader as="h3">
-                <Card.BodyHeaderOverline>Color - White</Card.BodyHeaderOverline>
-                <Card.BodyHeaderTitle>Article 2 </Card.BodyHeaderTitle>
+                <Card.BodyHeaderOverline>Customer service</Card.BodyHeaderOverline>
+                <Card.BodyHeaderTitle>Contact Customer service</Card.BodyHeaderTitle>
               </Card.BodyHeader>
               <Card.BodyDescription>
-                The slim cards are a collection of items. They will have the same height while on
-                the same row.
+                Contact our customer service for help with your questions
               </Card.BodyDescription>
               <Card.BodyAction>
                 <Card.BodyActionArrow />
                 <Link href="#article-2" variant="solid">
-                  Read more
+                  Get in touch
                 </Link>
               </Card.BodyAction>
             </Card.Body>
           </Card>
           <Card variant="slim" color="white">
             <Card.Media>
-              <Card.MediaImg alt="posten-bring" src={postenBringImage} />
+              <Card.MediaImg
+                alt="Posten delivery van with Bring cargo truck in the background"
+                src={postenBringImage}
+              />
             </Card.Media>
             <Card.Body>
               <Card.BodyHeader as="h3">
-                <Card.BodyHeaderOverline>Color - White</Card.BodyHeaderOverline>
-                <Card.BodyHeaderTitle>Article 3</Card.BodyHeaderTitle>
+                <Card.BodyHeaderOverline>Quote</Card.BodyHeaderOverline>
+                <Card.BodyHeaderTitle>Price enquiry</Card.BodyHeaderTitle>
               </Card.BodyHeader>
-              <Card.BodyDescription>
-                If there is a lot of contents in the card body, the card will be taller. The links
-                will be aligned at the bottom across the cards in a row.
-              </Card.BodyDescription>
+              <Card.BodyDescription>Request a quote for your business</Card.BodyDescription>
               <Card.BodyAction>
                 <Link href="#article-3" variant="solid">
                   <Card.BodyActionArrow />
-                  Read more
+                  Request a quote
                 </Link>
               </Card.BodyAction>
             </Card.Body>
           </Card>
           <Card variant="slim" color="white">
             <Card.Media>
-              <Card.MediaImg alt="posten-bring" src={postenBringImage} />
+              <Card.MediaImg
+                alt="Posten delivery van with Bring cargo truck in the background"
+                src={postenBringImage}
+              />
             </Card.Media>
             <Card.Body>
               <Card.BodyHeader as="h3">
-                <Card.BodyHeaderOverline>Color - White</Card.BodyHeaderOverline>
-                <Card.BodyHeaderTitle>Article 4</Card.BodyHeaderTitle>
+                <Card.BodyHeaderOverline>Customs</Card.BodyHeaderOverline>
+                <Card.BodyHeaderTitle>Get familiar with customs</Card.BodyHeaderTitle>
               </Card.BodyHeader>
-              <Card.BodyDescription>A short card.</Card.BodyDescription>
+              <Card.BodyDescription>
+                Familiarise yourself with regulations relating to customs and fees
+              </Card.BodyDescription>
               <Card.BodyAction>
                 <Link href="#article-4" variant="solid">
                   <Card.BodyActionArrow />
-                  Read more
+                  Find out about customs
                 </Link>
               </Card.BodyAction>
             </Card.Body>
