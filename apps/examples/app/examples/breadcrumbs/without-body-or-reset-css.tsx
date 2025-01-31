a16773d7069386b2ed99da7d41ae2935f49c27ef
Julian Jark <julian.jark@posten.no>
2025-01-31 14:21:24 +0100

    :memo: add breadcrumbs example for usage without reset.css and body.css

---
+++ apps/examples/app/examples/breadcrumbs/without-body-or-reset-css.tsx
@@ -0,0 +1,36 @@
+import "@postenbring/hedwig-css/dist/tokens.css";
+import "@postenbring/hedwig-css/dist/fonts.css";
+
+import "@postenbring/hedwig-css/dist/breadcrumbs.css";
+import "@postenbring/hedwig-css/dist/link.css";
+
+import { Breadcrumbs, Link } from "@postenbring/hedwig-react";
+
+function Example() {
+  return (
+    <>
+      <Breadcrumbs>
+        <li>
+          <Link href="https://www.posten.no">Track letters and parcels</Link>
+        </li>
+        <li>Shipment from SOMEONE YOU KNOW</li>
+      </Breadcrumbs>
+
+      {/* Just for example sake, since the white background comes from `body.css` */}
+      <style
+        dangerouslySetInnerHTML={{
+          __html: `body { background-color: var(--hds-ui-colors-white); }`,
+        }}
+      />
+    </>
+  );
+}
+
+export default Example;
+
+import type { ExampleConfig } from "..";
+export const config: ExampleConfig = {
+  index: 2,
+  breakpointIndicator: "top",
+  description: "When just importing the bare minimum css required.",
+};
