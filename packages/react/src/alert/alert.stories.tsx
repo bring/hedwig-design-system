b7208a36de46e00677eb09bc4e3c915494ec2ff0
Viggo Normann <v@iggo.no>
2025-05-23 15:44:48 +0200

    Add Alert component

---
+++ packages/react/src/alert/alert.stories.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable import/no-extraneous-dependencies -- storybook story */
+import type { Meta, StoryObj } from "@storybook/react";
+import { Alert } from ".";
+
+const meta: Meta<typeof Alert> = {
+  title: "Alert",
+  component: Alert,
+
+  argTypes: {
+    variant: {
+      options: ["success", "attention", "warning", "neutral", "info"],
+      control: {
+        type: "radio",
+      },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Alert>;
+export const Preview: Story = {
+  tags: ["!dev"],
+  args: {
+    variant: "success",
+    children: (
+      <>
+        <Alert.Title>Alert header</Alert.Title>
+        <Alert.Description>
+          Alert header Alert description. A more detailed explanation of whats happening, but not
+          too long.
+        </Alert.Description>
+      </>
+    ),
+  },
+};
