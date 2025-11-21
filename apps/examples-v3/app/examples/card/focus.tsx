c97ada0b2a565455de8cf54e365217d537806296
Espen Norderud <espen42@gmail.com>
2025-11-21 13:26:54 +0100

    Add buttons to focus card example

--- apps/examples-v3/app/examples/card/focus.tsx
+++ apps/examples-v3/app/examples/card/focus.tsx
@@ -1,5 +1,5 @@
 import "@postenbring/hedwig-css";
-import { Card, Container, Link } from "@postenbring/hedwig-react";
+import { Card, Container, Link, ButtonList, Button } from "@postenbring/hedwig-react";
 import postenBringImage from "../../assets/posten-bring.avif";
 import vectoriImage from "../../assets/customer-parcel.svg";
 
@@ -65,6 +65,45 @@ function Example() {
                 External link
               </Link>
             </Card.BodyAction>
+          </Card.Body>
+        </Card>
+
+        <h2>Buttons</h2>
+        <Card variant="focus">
+          <Card.Media>
+            <Card.MediaImg
+              alt="Use variant=crop on photographs, to make the image fill the frame completely"
+              src={postenBringImage}
+              variant="crop"
+            />
+          </Card.Media>
+          <Card.Body>
+            <Card.BodyHeader as="h2">
+              <Card.BodyHeaderOverline>Overline title</Card.BodyHeaderOverline>
+              <Card.BodyHeaderTitle>Import duties</Card.BodyHeaderTitle>
+            </Card.BodyHeader>
+            <Card.BodyDescription>
+              For more than 1 CTA button, use &lt;ButtonList variant=&quot;stretched&quot;, and add
+              an &lsquo;asChild&lsquo; prop to the surrounding Card.BodyActionRow.
+            </Card.BodyDescription>
+            <Card.BodyActionRow asChild>
+              <ButtonList variant="stretched">
+                <Card.BodyAction asChild>
+                  <Button fullWidth="mobile" asChild>
+                    <a href="https://www.postenbring.no" target="_blank" rel="noreferrer">
+                      Primary
+                    </a>
+                  </Button>
+                </Card.BodyAction>
+                <Card.BodyAction asChild>
+                  <Button fullWidth="mobile" variant="secondary" asChild>
+                    <a href="https://www.posten.no" target="_blank" rel="noreferrer">
+                      Secondary
+                    </a>
+                  </Button>
+                </Card.BodyAction>
+              </ButtonList>
+            </Card.BodyActionRow>
           </Card.Body>
         </Card>
 
