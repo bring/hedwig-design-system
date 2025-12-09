aab9f451255901aae8d437d7fd54f57bd4512357
Helle Hiorth-Schøyen <helle@solidsquare.no>
2025-12-09 09:09:13 +0100

    icons

--- packages/react/src/box/box.tsx
+++ packages/react/src/box/box.tsx
@@ -2,7 +2,7 @@ import { forwardRef, useCallback, useState } from "react";
 import { clsx } from "@postenbring/hedwig-css/typed-classname";
 import { Slot, Slottable } from "@radix-ui/react-slot";
 
-export function CloseIcon() {
+function CloseIcon() {
   return (
     <svg aria-hidden xmlns="http://www.w3.org/2000/svg" width={32} height={32} viewBox="0 0 32 32">
       <path
