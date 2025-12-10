98a9d669dedd511d62d5757244630d51383b3a6d
Viggo Normann <v@iggo.no>
2025-12-10 10:09:52 +0100

    Revert "Add @deprecated to variant for Box"
    This reverts commit e1da865de3258ae4dab1530a3b1483b8e915cdc4.

--- packages/react/src/box/box.tsx
+++ packages/react/src/box/box.tsx
@@ -34,7 +34,7 @@ export interface BoxProps extends React.HTMLAttributes<HTMLDivElement> {
   children?: React.ReactNode;
 
   /**
-   * @deprecated Posten/Bring-spesific variant of the box. NB: warning variant is deprecated, use data-color="warning" instead.
+   * Posten/Bring-spesific variant of the box. NB: warning variant is deprecated, use data-color="warning" instead.
    *
    * @default "light-grey"
    */
