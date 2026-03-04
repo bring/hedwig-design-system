60a92da0aa79dcf033f4b40ba0409358da2bd36b
Helle Hiorth-Schøyen <helle@solidsquare.no>
2026-03-04 13:59:51 +0100

    remove unused code

--- packages/react/src/modal/modal.stories.tsx
+++ packages/react/src/modal/modal.stories.tsx
@@ -17,26 +17,22 @@ export const Preview: Story = {
   tags: ["!dev"],
   render: function Render(args) {
     const modalRef = useRef<HTMLDialogElement>(null);
-    const onClose = () => modalRef.current?.close();
     const onMainAction = () => modalRef.current?.close();
 
     return (
       <>
         <Button onClick={() => modalRef.current?.showModal()}>Open Modal</Button>
         <Modal ref={modalRef} {...args}>
-          <Modal.Header>Dialog header</Modal.Header>
+          <Modal.Header>Case registered</Modal.Header>
           <Modal.Content>
             <p>
-              Dialog header Dialog description - a description of what is about to happen and maybe
-              something about the consequences.
+              Your case has been registered. Reference number: #123456 something about the
+              consequences.
             </p>
           </Modal.Content>
           <Modal.Footer>
             <HStack gap="16" wrap>
-              <Button onClick={onMainAction}>Main action</Button>
-              <Button variant="secondary" onClick={onClose}>
-                Cancel
-              </Button>
+              <Button onClick={onMainAction}>OK</Button>
             </HStack>
           </Modal.Footer>
         </Modal>
