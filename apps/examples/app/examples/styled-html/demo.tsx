d582057bfee1f170531962b71c83214efefcc928
Helle Hiorth-Schøyen <helle@solidsquare.no>
2026-03-06 09:26:49 +0100

    update doc for styledHtml

--- apps/examples/app/examples/styled-html/demo.tsx
+++ apps/examples/app/examples/styled-html/demo.tsx
@@ -5,169 +5,108 @@ function Example() {
   return (
     <Container>
       <StyledHtml style={{ padding: "var(--hds-spacing-24)" }}>
-        <h1>Pakkebokser</h1>
+        <h1>Weights and dimensions for domestic shipments</h1>
         <p>
-          Pakkeboksene er selvbetjente, og er plassert slik at de er mulig å bruke hele døgnet. Du
-          trenger bare mobilen din og Posten-appen for å åpne lukene.
+          Posten has different maximum and minimum dimensions for letters and parcels that are to be
+          sent. This is because the items must go through various mechanical processing stages
+          before they reach the recipient. Measure and weigh your item so that we can sort it as
+          quickly as possible. You can frank your items with stamps, write a code on them, or print
+          an address label.
         </p>
+        <h2>Mail franked with stamps or code</h2>
+
+        <p>Find a measuring tape or ruler and a baking scale or bathroom scale.</p>
+
+        <h3>Maximum size for letters with stamps</h3>
 
         <ul>
-          <li>
-            <p>
-              <i>
-                <strong>
-                  Handler du fra en VOEC-registrert nettbutikk får du se totalprisen når du handler.
-                </strong>
-              </i>
-            </p>
-          </li>
+          <li>Length + width + thickness: 90 cm</li>
+          <li>Length: 60 cm</li>
+          <li>Weight: 2 kilos</li>
         </ul>
-        <p>
-          Akkurat nå er det satt ut pakkebokser over 1800 steder i landet, og fremover vil det komme
-          enda flere.&nbsp;
-        </p>
+
+        <p>If the letter is larger or heavier than this, it can be sent as Norgespakke™.</p>
 
         <p>
-          <a href="/motta/fleksibelt/pakkeboks" title="Les mer og finn Pakkeboks">
-            Her kan du finne din nærmeste Pakkeboks
-          </a>
+          NB: If the letter is more than 35.3 cm long or more than 25 cm wide, it is priced as more
+          than 7 cm thick.{" "}
+          <a href="content://aa151440-df95-4b07-be5c-4de700cf0c98">See the price list for 2026</a>.
         </p>
 
-        <h2 className="hds-text-h3">Slik bruker du Pakkeboks:</h2>
-
-        <p>
-          For å bruke pakkeboksene må du&nbsp;
-          <a href="/posten-app" target="_blank" title="Last ned Posten-appen">
-            laste ned Posten-appen
-          </a>
-          &nbsp;på mobilen din. Med appen kan du:
-        </p>
+        <h3>Maximum size for letters with a digital stamp</h3>
 
         <ul>
-          <li>Åpne luke for å hente pakke</li>
-          <li>Reservere og åpne luke når du skal sende pakke</li>
-          <li>Velge om du vil ha pakker kun levert i de midterste lukene</li>
-          <li>Dele pakkene dine&nbsp;med andre, slik at de kan hente eller sende dem for deg</li>
+          <li>Maximum weight: 2 kilos</li>
+          <li>Maximum size: 35,3 x 25 x 2&nbsp;cm</li>
+          <li>Minimum size: 9 x 14 cm</li>
         </ul>
 
-        <h2>Hente pakker</h2>
-
         <p>
-          Du får varsel i Posten-appen når du har en pakke som skal hentes i Pakkeboks. Slik gjør
-          du:
+          For larger shipments, <a href="http://www.posten.no">see Norgespakke™</a>.
         </p>
+
+        <h3>Minimum letter size</h3>
+
+        <p>The minimum size for all letters&nbsp; 9 x 14 cm.</p>
+
+        <h3>Maximum size for roll</h3>
 
         <ul>
-          <li>Stå ved pakkeboksen og åpne appen. Husk å ha på Bluetooth</li>
-          <li>Appen kobler til riktig boks, og viser hvilken luke pakken din er i</li>
-          <li>Trykk på Åpne luke, og ta ut pakken din.</li>
-          <li>Steng luken og lukk appen</li>
+          <li>Length: 90 cm</li>
+          <li>Length + double diameter: 104 cm</li>
+          <li>Minimum roll size</li>
+          <li>Length: 10 cm</li>
+          <li>Length + double diameter: 17 cm</li>
         </ul>
 
-        <h2>Velge høyde på luke</h2>
+        <p>
+          <a href="http://www.posten.no">Read more about letters in Norway.</a>
+        </p>
 
-        <p>Slik velger du at du vil ha pakkene kun i midterste luker:</p>
+        <h2>Parcels</h2>
+
+        <h3>Norgespakke™ small</h3>
 
         <ul>
-          <li>Du må ha nyeste versjon av Posten-appen</li>
-          <li>Åpne appen, og trykk på «Profil»</li>
-          <li>Velg «Levering i Pakkeboks», deretter&nbsp;«Luker på midten»</li>
-          <li>Lagre valget ditt</li>
+          <li>Maximum weight: up to 5 kg</li>
+          <li>Maximum size: 35,3 x 25 x 12 cm</li>
+          <li>Minimum size: 9 x 14 cm</li>
         </ul>
 
-        <h2>Sende fra Pakkeboks</h2>
-
         <p>
-          Nå kan du bruke Posten-appen for å reservere luker i pakkeboksen når du skal sende{" "}
-          <a
-            href="/sende/i-norge/norgespakke/norgespakke-0-5-kg"
-            target="_blank"
-            title="Les mer om Norgespakke™ liten"
-          >
-            Norgespakke™ liten
-          </a>
-          .
+          <a href="http://www.posten.no">Read more about Norgespakke™ small.</a>
         </p>
 
-        <p>Slik reserverer du luke for å sende pakke:</p>
+        <h3>Norgespakke™ large</h3>
 
         <ul>
-          <li>
-            Du må ha Posten-appen for å bestille sending og&nbsp;holde av en&nbsp;luke i pakkeboksen
-          </li>
-          <li>Velg&nbsp;Norgespakke™ liten, og følg stegene for kjøp av frakt</li>
-          <li>Velg innlevering på Posten, og fullfør kjøpet</li>
-          <li>
-            Du trenger ikke reservere luke rett etter kjøp, dette kan du gjøre innenfor 2 timer før
-            du skal sende pakken
-          </li>
-          <li>Pakken din blir hentet senest neste virkedag</li>
+          <li>Maximum weight: 35 kg per parcel</li>
+          <li>Maximum size: 120 x 60 x 60 cm</li>
+          <li>Minimum size: 23 x 13 x 1 cm</li>
         </ul>
 
-        <h2>Returnere fra Pakkeboks</h2>
+        <p>
+          If the parcel is more than 120 cm long, has a width of more than 60 cm and/or a height of
+          more than 60 cm, there is a handling surcharge of 186.– kroner.
+        </p>
+
+        <p>The parcel can then be up to 240 cm long and length + circumference up to 360 cm.</p>
+
+        <p>
+          <a href="http://www.posten.no">Read more about Norgespakke™ large.</a>
+        </p>
+
+        <h3>Norgespakke™ express</h3>
 
         <ul>
-          <li>
-            Du må ha&nbsp;<a href="https://www.posten.no/posten-app">Posten-appen</a>&nbsp;for å
-            reservere en luke.
-          </li>
-          <li>Velg retur i appen og følg stegene videre</li>
-          <li>Fest&nbsp;adresselappen for retur&nbsp;midt på pakken med strekkoden godt synlig</li>
-          <li>Luken holdes av i to timer fra du reserverer den.</li>
-          <li>Du kan reservere en luke&nbsp;når som helst</li>
+          <li>Maximum weight: 35 kg per parcel</li>
+          <li>Maximum size: 120 x 60 x 60 cm</li>
+          <li>Minimum size: 23 x 13 x 1 cm</li>
         </ul>
 
-        <p>Er det noe du lurer på?</p>
-
         <p>
-          Se flere <a href="/kundeservice/pakkeboks-hjelp">spørsmål og svar om Pakkeboks</a>.
+          <a href="http://www.posten.no">Read more about Norgespakke™ express.</a>
         </p>
-
-        <figure>
-          <img src="https://placedog.net/500/280" alt="A very good dog" />
-          <figcaption>Dogs are the best</figcaption>
-        </figure>
-
-        <h2>Et sitat</h2>
-        <p>
-          På denne måten slipper du å tenke noe mer på importfortolling, for nettbutikken betaler
-          avgiftene direkte til Skatteetaten. Dette gjelder nettbutikker som er registrert i VOEC
-          (les mer om hva det er lenger ned på siden).
-        </p>
-        <blockquote>
-          <p>... but they&rsquo;ll never take our freedom!</p>
-          <footer>William Wallace</footer>
-        </blockquote>
-        <p>
-          Samtidig som Stortinget avvikler tollfri handel, har Posten utviklet nye, digitale
-          fortollingstjenester. Disse er spesielt tilpasset netthandel og kjøp av varer med lav
-          verdi fra utlandet, og gjør at prisene på fortolling til privatkunder blir mye lavere enn
-          tidligere. De nye prisene finner du lenger ned på denne siden.
-        </p>
-
-        <h2>En tabell</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Sted</th>
-              <th>Antall</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Oslo</td>
-              <td>100</td>
-            </tr>
-            <tr>
-              <td>Bergen</td>
-              <td>50</td>
-            </tr>
-            <tr>
-              <td>Trondheim</td>
-              <td>30</td>
-            </tr>
-          </tbody>
-        </table>
       </StyledHtml>
     </Container>
   );
