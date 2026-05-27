17330ba1bc06c0c84465068bf1342e2bb97e5fc6
henning-digipost <henning.bang.halvorsen@posten.no>
2026-05-27 10:31:39 +0200

    Buxfix: ButtonList stretched with icons
    Buttons with icons in a ButtonList with variant stretched did not center the text and icon.
    Exclude svg and path from css selector that sets flex properties.

---
+++ .changeset/social-memes-brake.md
@@ -0,0 +1,5 @@
+---
+"@postenbring/hedwig-css": patch
+---
+
+Visual bugfix for buttons with icons in buttonlist variant stretched
---
+++ apps/examples/app/examples/button-list/stretched-with-icons.tsx
@@ -0,0 +1,27 @@
+import "@postenbring/hedwig-css";
+import { Button, ButtonList } from "@postenbring/hedwig-react";
+import { GlobeIcon, EnvelopeIcon } from "../../assets/icon-examples";
+
+function Example() {
+  return (
+    <ButtonList variant="stretched">
+      <Button icon="trailing">
+        First button <GlobeIcon />
+      </Button>
+      <Button variant="secondary" icon="trailing">
+        Second button <GlobeIcon />
+      </Button>
+      <Button variant="tertiary" icon="leading">
+        <EnvelopeIcon /> Third button
+      </Button>
+    </ButtonList>
+  );
+}
+
+export default Example;
+
+import type { ExampleConfig } from "..";
+export const config: ExampleConfig = {
+  index: 1,
+  layout: "centered-fullwidth",
+};
