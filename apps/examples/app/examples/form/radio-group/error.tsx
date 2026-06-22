2720cbfcb9f8da65612d3ea05db9d36808057baf
Helle Hiorth-Schøyen <helle@solidsquare.no>
2026-06-22 13:13:48 +0200

    add example on errorMessage in radioGroup

---
+++ apps/examples/app/examples/form/radio-group/error.tsx
@@ -0,0 +1,25 @@
+import "@postenbring/hedwig-css";
+import { RadioGroup, RadioButton } from "@postenbring/hedwig-react";
+
+function Example() {
+  return (
+    <RadioGroup name="group1" errorMessage="Something's wrong">
+      <RadioGroup.Legend>Default radio group</RadioGroup.Legend>
+      <RadioGroup.Description>This is a description</RadioGroup.Description>
+      <RadioButton defaultChecked value="Hello">
+        Hello
+      </RadioButton>
+      <RadioButton value="Hello">Hello</RadioButton>
+      <RadioButton value="Hello">Hello</RadioButton>
+    </RadioGroup>
+  );
+}
+
+export default Example;
+
+import type { ExampleConfig } from "../..";
+export const config: ExampleConfig = {
+  description: `Radio buttons should be grouped in a <code>RadioGroup</code> with a given <code>name</code>`,
+  index: 2,
+  layout: "centered-fullwidth",
+};
