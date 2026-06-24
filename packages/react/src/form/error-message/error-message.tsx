9f66a018c84873916fefb57c66d5ddb86bec21db
Viggo Normann <v@iggo.no>
2026-06-24 13:03:26 +0200

    Deprecate ErrorMessage

--- packages/react/src/form/error-message/error-message.tsx
+++ packages/react/src/form/error-message/error-message.tsx
@@ -1,12 +1,14 @@
 import { forwardRef, type ReactNode } from "react";
 import { ValidationMessage } from "../validation-message";
 
+/** @deprecated Use ValidationMessageProps instead */
 export interface ErrorMessageProps extends React.HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
   id: string;
   className?: string;
 }
 
+/** @deprecated Use ValidationMessage with data-color="error" instead */
 export const ErrorMessage = forwardRef<HTMLDivElement, ErrorMessageProps>(
   ({ children, id, className, ...rest }, ref) => (
     <ValidationMessage id={id} data-color="error" className={className} {...rest} ref={ref}>
