52bf7588f37d80efa97ee29f7366be2587fd8d52
Viggo Normann <v@iggo.no>
2026-07-03 11:39:05 +0200

    Refactor SearchWrapper component: Simplify props and remove unused elements

--- packages/react/src/form/search-wrapper/search-wrapper.tsx
+++ packages/react/src/form/search-wrapper/search-wrapper.tsx
@@ -1,29 +1,15 @@
 import { forwardRef } from "react";
-import type { ReactNode, CSSProperties } from "react";
+import type { HTMLAttributes } from "react";
 import { clsx } from "@postenbring/hedwig-css/typed-classname";
 
-export interface SearchWrapperProps {
-  id?: string;
+export interface SearchWrapperProps extends HTMLAttributes<HTMLDivElement> {
   className?: string;
-  style?: CSSProperties;
-  children: ReactNode;
 }
 
-export const SearchWrapper = forwardRef<HTMLDivElement, SearchWrapperProps>(function SearchWrapper(
-  { id, className, style, children, ...rest },
-  ref,
-) {
-  return (
-    <div
-      id={id}
-      className={clsx("hds-search-wrapper", className as undefined)}
-      ref={ref}
-      style={style}
-      {...rest}
-    >
-      {children}
-    </div>
-  );
-});
+export const SearchWrapper = forwardRef<HTMLDivElement, SearchWrapperProps>(
+  ({ className, ...rest }, ref) => (
+    <div className={clsx("hds-search-wrapper", className as undefined)} ref={ref} {...rest} />
+  ),
+);
 
 SearchWrapper.displayName = "SearchWrapper";
