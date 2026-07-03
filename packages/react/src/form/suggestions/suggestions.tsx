6497ef66a625a49e2d98056853423b542b88985d
Viggo Normann <v@iggo.no>
2026-07-03 06:26:01 +0200

    refactor: simplify SuggestionProps and update Suggestions component structure

--- packages/react/src/form/suggestions/suggestions.tsx
+++ packages/react/src/form/suggestions/suggestions.tsx
@@ -1,36 +1,30 @@
 import { forwardRef } from "react";
-import type { ReactNode, CSSProperties } from "react";
+import type { HTMLAttributes } from "react";
 import { clsx } from "@postenbring/hedwig-css/typed-classname";
 
-export interface SuggestionProps {
-  id?: string;
-  className?: string;
-  style?: CSSProperties;
-  children: ReactNode;
-  size?: "small" | "large" | undefined;
+export interface SuggestionProps extends HTMLAttributes<HTMLUListElement> {
+  /**
+   * Sets the size of the items
+   *
+   * @default "default"
+   */
+  size?: "default" | "small";
 }
 
-export const Suggestions = forwardRef<HTMLDivElement, SuggestionProps>(function Suggestions(
-  { id, className, style, size, children, ...rest },
-  //ref,
-) {
-  return (
-    <ul
-      id={id}
-      className={clsx(
-        "hds-suggestions",
-        {
-          [`hds-suggestions--${size}`]: size,
-        },
-        className as undefined,
-      )}
-      //ref={ref}
-      style={style}
-      {...rest}
-    >
-      {children}
-    </ul>
-  );
-});
+export const Suggestions = forwardRef<HTMLUListElement, SuggestionProps>(
+  ({ className, size = "default", ...rest }, ref) => {
+    return (
+      <ul
+        className={clsx(
+          "hds-suggestions",
+          size !== "default" && `hds-suggestions--${size}`,
+          className as undefined,
+        )}
+        ref={ref}
+        {...rest}
+      />
+    );
+  },
+);
 
 Suggestions.displayName = "Suggestions";
