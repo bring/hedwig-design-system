425f4f25753f685091dcf42ab502d7a7e3e967c7
Viggo Normann <v@iggo.no>
2026-07-03 10:43:28 +0200

    Refactor Select component: Let some props be handled by existing functionality

--- packages/react/src/form/select/select.tsx
+++ packages/react/src/form/select/select.tsx
@@ -1,50 +1,41 @@
-import { forwardRef, type ReactNode, type SelectHTMLAttributes } from "react";
+import { forwardRef, type SelectHTMLAttributes } from "react";
 import { clsx } from "@postenbring/hedwig-css/typed-classname";
 import { InputGroup } from "../input-group";
 import type { InputGroupProps } from "../input-group";
 
 export type SelectProps = Omit<
   InputGroupProps & Omit<SelectHTMLAttributes<HTMLSelectElement>, "size">,
-  "readOnly" | "children"
-> & { children: ReactNode };
+  "readOnly"
+>;
 
-export const Select = forwardRef<HTMLSelectElement, SelectProps>(function Select(
-  {
-    className,
-    size,
-    validationMessage,
-    validationMessageProps,
-    errorMessage,
-    labelProps,
-    label,
-    id,
-    style,
-    disabled,
-    "data-color": dataColor,
-    children,
-    ...rest
-  },
-  ref,
-) {
-  return (
+export const Select = forwardRef<HTMLSelectElement, SelectProps>(
+  (
+    {
+      className,
+      size,
+      validationMessage,
+      validationMessageProps,
+      errorMessage,
+      labelProps,
+      label,
+      "data-color": dataColor,
+      ...rest
+    },
+    ref,
+  ) => (
     <InputGroup
       className={clsx("hds-select", className as undefined)}
-      disabled={disabled}
       validationMessage={validationMessage}
       validationMessageProps={validationMessageProps}
       errorMessage={errorMessage}
-      id={id}
       label={label}
       labelProps={labelProps}
-      style={style}
       size={size}
       data-color={dataColor}
     >
-      <select disabled={disabled} ref={ref} {...rest}>
-        {children}
-      </select>
+      <select ref={ref} {...rest} />
     </InputGroup>
-  );
-});
+  ),
+);
 
 Select.displayName = "Select";
